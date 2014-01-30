e16915d72a6db04c697098cc31e6a1a51e433123
Sam Goshorn <sam@cali.org>
2014-01-30 15:17:27 -0600

    cleanup

--- js/A2J_ViewerApp.js
+++ js/A2J_ViewerApp.js
@@ -82,7 +82,6 @@ function gotoPageView(destPageName)
 }
 
 function main(){
-	$('.viewerenv').text(gEnv+' '+CONST.A2JVersionNum+" "+CONST.A2JVersionDate);
 	loadGuideFile(templateURL,"");
 }
 $(document).ready(main);

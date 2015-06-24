2f1975e60025cd7cca7f18fca4735aae56d43dac
Manuel Mujica <evilsaurer@gmail.com>
2015-06-24 12:30:10 -0300

    Removing the php dependency of the JS app.
    - Add some npm packages deps.
    - Move author css files to author/styles folder.
    - Add some dotfiles to configure editorconfig, jshint and jscs.
    - Add some fixtures to intercept the ajax call to the php web service.

--- js/author/A2J_Guides.js
+++ js/author/A2J_Guides.js
@@ -1,11 +1,11 @@
 /*
 	A2J Author 5 * Justice * justicia * 正义 * công lý * 사법 * правосудие
 	All Contents Copyright The Center for Computer-Assisted Legal Instruction
-	
+
 	Authoring App Guides GUI
 	Guide mangement including loading/saving/archive, display list.
 	2015 04/15/2013, 	05/2014
-	
+
 */
 
 /* global gGuidePath,gPage,gGuide,gUserID,gGuideID,gUserNickName */
@@ -22,7 +22,7 @@ function guideLoaded(data)
 	try {
 		cajaDataXML=$(jQuery.parseXML(data.guide));
 	}
-	catch (e) { 
+	catch (e) {
 		setProgress('');
 		dialogAlert({title:'Error occurred loading guide #'+data.gid,body:'Unable to load XML' +"\n"+String.substr(String(e).asHTML(),0,99)});
 		return;
@@ -31,7 +31,7 @@ function guideLoaded(data)
 	gGuide =  parseXML_Auto_to_CAJA(cajaDataXML);
 	//$('li.guide[gid="'+gGuideID+'"]').html(gGuide.title);
 
-	gGuidePath=urlSplit(data.path).path; 
+	gGuidePath=urlSplit(data.path).path;
 	guideStart('');
 	setProgress('');
 }
@@ -45,10 +45,10 @@ function guideSave(onFinished)
 		var xml = exportXML_CAJA_from_CAJA(gGuide);
 		if (xml!==gGuide.lastSaveXML) {
 			gGuide.lastSaveXML=xml;
-			
+
 			// 01/14/2015 included JSON form of guide XML
-			var guideJSON_str=guide2JSON_Mobile(gGuide);	
-			
+			var guideJSON_str=guide2JSON_Mobile(gGuide);
+
 			ws( {cmd:'guidesave',
 				gid:gGuideID,
 				guide: xml,
@@ -78,31 +78,31 @@ function loadNewGuidePrep(guideFile,startTabOrPage)
 
 
 function guideStart(startTabOrPage)
-{ 
+{
 	if (startTabOrPage === ''){
 		startTabOrPage='tabsPages';//'tabsAbout';
 	}
-	
-	
-	
+
+
+
 	$('#splash').hide();
 	$('#authortool').removeClass('hidestart');//.addClass('authortool').show
-	
+
 	//$('#tabviews').tabs( { disabled:false});
 	$('#tabsVariables .tabContent, #tabsLogic  .tabContent, #tabsSteps .tabContent, #tabsAbout .tabContent, #tabsClauses .tabContent, #tabsText .tabContent').html("");
-	
+
 	if (makestr(startTabOrPage)===""){
 		startTabOrPage="PAGE "+(gGuide.firstPage);
 	}
 	//trace("Starting location "+startTabOrPage);
-	
+
 	gotoTabOrPage(startTabOrPage);
 	updateTOC();
-	
+
 	//$('#guidepanel ul li a:first').html(gGuide.title);
 	$('#guidetitle').html(gGuide.title);
-	
-	
+
+
 	// ### Upload file(s) to current guide
 	$('#fileupload').addClass('fileupload-processing');
 	if (gGuideID!==0) {
@@ -121,10 +121,10 @@ function guideStart(startTabOrPage)
 		updateAttachmentFiles();
 	}
 
-	
+
 	buildMap();
-	
-	
+
+
 	if (gEnv!=='' && gStartArgs.getDataURL!=='') {
 		localGuidePlay();
 		return;
@@ -133,10 +133,10 @@ function guideStart(startTabOrPage)
 
 
 function blankGuide()
-{	// 2014-07-24 Create exact duplicate of A2J 4's New Interview. 
+{	// 2014-07-24 Create exact duplicate of A2J 4's New Interview.
 	//var A2J4_XML='<?xml version="1.0" encoding="UTF-8" ?><!DOCTYPE Access2Justice_1><TEMPLATE><TITLE>My Interview</TITLE><AUTHOR>Anonymous</AUTHOR><SENDFEEDBACK>false</SENDFEEDBACK><DESCRIPTION>This is a description of my interview.</DESCRIPTION><JURISDICTION>Jurisdiction of my interview</JURISDICTION><LANGUAGE>en</LANGUAGE><AVATAR>blank</AVATAR><VERSION>7/24/2014</VERSION><A2JVERSION>2012-04-19</A2JVERSION><HISTORY>Interview created 7/24/2014</HISTORY><QUESTIONCOUNTER>4</QUESTIONCOUNTER><FIRSTQUESTION>1</FIRSTQUESTION><VARIABLES><VARIABLE SCOPE="Interview" NAME="User Gender" TYPE="Text"><COMMENT>User&apos;s gender will be used to display appopriate avatar.</COMMENT></VARIABLE><VARIABLE SCOPE="Interview" NAME="User Avatar" TYPE="Text" /><VARIABLE SCOPE="Interview" NAME="Client first name TE" TYPE="Text" /><VARIABLE SCOPE="Interview" NAME="Client middle name TE" TYPE="Text" /><VARIABLE SCOPE="Interview" NAME="Client last name TE" TYPE="Text" /></VARIABLES><STEPS><STEP NUMBER="0"><TEXT>ACCESS TO JUSTICE</TEXT></STEP><STEP NUMBER="1"><TEXT>DO YOU QUALIFY?</TEXT></STEP><STEP NUMBER="2"><TEXT>DO YOU AGREE?</TEXT></STEP><STEP NUMBER="3"><TEXT>YOUR INFORMATION</TEXT></STEP></STEPS><QUESTIONS><QUESTION ID="1" STEP="0" MAPX="15" MAPY="146" NAME="1-Introduction"><TEXT><P><FONT>This is the introduction.</FONT></P></TEXT><BUTTONS><BUTTON NEXT="2" /></BUTTONS></QUESTION><QUESTION ID="2" STEP="0" MAPX="15" MAPY="396" NAME="2-Name"><TEXT><P><FONT>Enter your name.</FONT></P></TEXT><FIELDS><FIELD TYPE="text" ORDER="ASC"><LABEL>First:</LABEL><NAME>Client first name TE</NAME><INVALIDPROMPT>You must type a response in the highlighted space before you can continue.</INVALIDPROMPT></FIELD><FIELD TYPE="text" OPTIONAL="true" ORDER="ASC"><LABEL>Middle:</LABEL><NAME>Client middle name TE</NAME><INVALIDPROMPT>You must type a response in the highlighted space before you can continue.</INVALIDPROMPT></FIELD><FIELD TYPE="text" ORDER="ASC"><LABEL>Last:</LABEL><NAME>Client last name TE</NAME><INVALIDPROMPT>You must type a response in the highlighted space before you can continue.</INVALIDPROMPT></FIELD></FIELDS><BUTTONS><BUTTON NEXT="3" /></BUTTONS></QUESTION><QUESTION ID="3" STEP="0" MAPX="15" MAPY="646" NAME="3-Gender"><TEXT>Choose your gender.</TEXT><FIELDS><FIELD TYPE="gender"><LABEL>Gender:</LABEL><NAME>User Gender</NAME></FIELD></FIELDS><BUTTONS><BUTTON NEXT="4" /></BUTTONS></QUESTION><QUESTION ID="4" STEP="1" MAPX="15" MAPY="896" NAME="1-Question 1"><TEXT>Text of my first question goes here.</TEXT><BUTTONS><BUTTON /></BUTTONS></QUESTION></QUESTIONS><POPUPS /></TEMPLATE>';
 	var A2J4_XML='<?xml version="1.0" encoding="UTF-8" ?><!DOCTYPE Access2Justice_1><TEMPLATE><TITLE>My Interview</TITLE><AUTHOR>Anonymous</AUTHOR><SENDFEEDBACK>false</SENDFEEDBACK><DESCRIPTION>This is a description of my interview.</DESCRIPTION><JURISDICTION>Jurisdiction of my interview</JURISDICTION><LANGUAGE>en</LANGUAGE><AVATAR>blank</AVATAR><VERSION>7/24/2014</VERSION><A2JVERSION>2012-04-19</A2JVERSION><HISTORY>Interview created 7/24/2014</HISTORY><QUESTIONCOUNTER>4</QUESTIONCOUNTER><FIRSTQUESTION>1</FIRSTQUESTION><VARIABLES><VARIABLE SCOPE="Interview" NAME="User Gender" TYPE="Text"><COMMENT>User&apos;s gender will be used to display appopriate avatar.</COMMENT></VARIABLE><VARIABLE SCOPE="Interview" NAME="User Avatar" TYPE="Text" /><VARIABLE SCOPE="Interview" NAME="Client first name TE" TYPE="Text" /><VARIABLE SCOPE="Interview" NAME="Client middle name TE" TYPE="Text" /><VARIABLE SCOPE="Interview" NAME="Client last name TE" TYPE="Text" /></VARIABLES><STEPS><STEP NUMBER="0"><TEXT>ACCESS TO JUSTICE</TEXT></STEP><STEP NUMBER="1"><TEXT>DO YOU QUALIFY?</TEXT></STEP><STEP NUMBER="2"><TEXT>DO YOU AGREE?</TEXT></STEP><STEP NUMBER="3"><TEXT>YOUR INFORMATION</TEXT></STEP></STEPS><QUESTIONS><QUESTION ID="1" STEP="0" MAPX="13.25" MAPY="155.45" NAME="1-Introduction"><TEXT><P><FONT>This is the introduction.</FONT></P></TEXT><BUTTONS><BUTTON NEXT="2" /></BUTTONS></QUESTION><QUESTION ID="2" STEP="0" MAPX="22.75" MAPY="401.85" NAME="2-Name"><TEXT><P><FONT>Enter your name.</FONT></P></TEXT><FIELDS><FIELD TYPE="text" ORDER="ASC"><LABEL>First:</LABEL><NAME>Client first name TE</NAME><INVALIDPROMPT>You must type a response in the highlighted space before you can continue.</INVALIDPROMPT></FIELD><FIELD TYPE="text" OPTIONAL="true" ORDER="ASC"><LABEL>Middle:</LABEL><NAME>Client middle name TE</NAME><INVALIDPROMPT>You must type a response in the highlighted space before you can continue.</INVALIDPROMPT></FIELD><FIELD TYPE="text" ORDER="ASC"><LABEL>Last:</LABEL><NAME>Client last name TE</NAME><INVALIDPROMPT>You must type a response in the highlighted space before you can continue.</INVALIDPROMPT></FIELD></FIELDS><BUTTONS><BUTTON NEXT="3" /></BUTTONS></QUESTION><QUESTION ID="3" STEP="0" MAPX="498.9" MAPY="19.9" NAME="3-Gender"><TEXT>Choose your gender.</TEXT><FIELDS><FIELD TYPE="gender"><LABEL>Gender:</LABEL><NAME>User Gender</NAME></FIELD></FIELDS><BUTTONS><BUTTON NEXT="4" /></BUTTONS></QUESTION><QUESTION ID="4" STEP="1" MAPX="510.15" MAPY="396.9" NAME="1-Question 1"><TEXT><P><FONT>Text of my first question goes here.</FONT></P></TEXT><BUTTONS><BUTTON /></BUTTONS></QUESTION></QUESTIONS><POPUPS /></TEMPLATE>';
-	
+
 	/** @type {TGuide} */
 	var guide = parseXML_Auto_to_CAJA($(jQuery.parseXML(A2J4_XML)));
 	var today = jsDate2mdy(today2jsDate());
@@ -188,7 +188,7 @@ function openSelectedGuide()
 }
 
 function archiveSelectedGuide()
-{	// 2014-08-28 Delete the currently selected guide	
+{	// 2014-08-28 Delete the currently selected guide
 	var $li=$('li.guide.'+SELECTED).first();
 	var name = $('span.title',$li).text();
 	dialogConfirmYesNo({title:'Delete interview',message:'Would you like to delete '+name+'?',height:300,name:name,
@@ -201,7 +201,7 @@ function archiveSelectedGuide()
 			}
 			ws({cmd:'guidearchive',gid:gid},wsListGuides);
 		}});
-	
+
 }
 function wsListGuides()
 {
@@ -213,7 +213,7 @@ function listGuides(data)
 {
 	var blank = {id:'a2j', title:'Blank Interview'};
 	//gGuideID=0;
-	
+
 	var guideListNew =['<li class=guide gid="' + blank.id + '">' + blank.title + '</li>'];
 	var guideListMy = [];
 	var guideListSamples = [];
@@ -221,7 +221,7 @@ function listGuides(data)
 		/*** @param {{owned,id,title}} g */
 		function(key,g)
 		{
-			
+
 			var size='?';
 			var modified='?';
 			//var pagecount='?';
@@ -252,12 +252,12 @@ function listGuides(data)
 	);
 	$('#guideListNew').html(guideListNew.join(''));
 	$('#guideListMy').html(guideListMy.join(''));
-	
+
 	$('#guideListSamples').html(guideListSamples.join(''));
 
 	$('li.guide').click(function(){$('li.guide').removeClass(SELECTED);$(this).addClass(SELECTED);});
 	$('li.guide').dblclick(openSelectedGuide);
-	
+
 	//$('#welcome').dialog('open');
 }
 
@@ -275,7 +275,7 @@ TGuide.prototype.HotDocsAnswerSetFromCMPXML=function(cmp)
 	*/
 	/** @type {TGuide} */
 	var guide=this;
-	
+
 	//cmp.find('hd\\:components').children().each(function() //  doesn't work in chrome
 
 	cmp.find("*").each(function()
@@ -289,14 +289,14 @@ TGuide.prototype.HotDocsAnswerSetFromCMPXML=function(cmp)
 				// hd:text name="AGR Petitioner Mother-Father-TE" askAutomatically="false" saveAnswer="false" warnIfUnanswered="false"/>
 				guide.varCreate(name,CONST.vtText,repeating,comment);
 				break;
-			
-			
-			
+
+
+
 			case 'hd:trueFalse':
 				// <hd:trueFalse name="Petitioner unemployment TF">
 				guide.varCreate(name,CONST.vtTF,repeating,comment);
 				break;
-			
+
 			case 'hd:date':
 				//	<hd:date name="Date child came into petitioner care DA">
 				//		<hd:defFormat>June 3, 1990</hd:defFormat>
@@ -305,17 +305,17 @@ TGuide.prototype.HotDocsAnswerSetFromCMPXML=function(cmp)
 				//	</hd:date>
 				guide.varCreate(name,CONST.vtDate,repeating,comment);
 				break;
-			
+
 			case 'hd:number':
 				//	<hd:number name="Putative children counter" askAutomatically="false" saveAnswer="false" warnIfUnanswered="false"/>
 				guide.varCreate(name,CONST.vtNumber,repeating,comment);
 				break;
-			
+
 			case 'hd:multipleChoice':
 				// <hd:multipleChoice name="Child gender MC" askAutomatically="false">
 				guide.varCreate(name,CONST.vtMC,repeating,comment);
 				break;
-			
+
 			case 'hd:computation':
 				//	<hd:computation name="Any parent address not known CO" resultType="trueFalse">
 				// <hd:computation name="A minor/minors aff of due dliligence CO" resultType="text">
@@ -332,7 +332,7 @@ TGuide.prototype.HotDocsAnswerSetFromCMPXML=function(cmp)
 				break;
 		}
 	});
-	
+
 	guide.noviceTab('tabsVariables',true);
 };
 /* */

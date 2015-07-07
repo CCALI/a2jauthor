ec490dc9f6b2e273b5410044eb953c2f420fbac2
Manuel Mujica <evilsaurer@gmail.com>
2015-07-07 10:14:41 -0300

    Move author app markup to its own template.
    This changes the way the app is bootstrapped, now, we load all of the
    author markup from `app.stache` (actually from app_template.js since we
    need the bundle for build purposes), then we render this template into a
    div#author-app element and once that's done, the legacy js code is
    loaded.
    
    - Set transpiler to babel insted of traceur
    - Add bundles for the dynamically loaded modules (app_template and
      src/src)
    
    Closes #44.

---
+++ js/author/app_template.js
@@ -0,0 +1,17 @@
+/**
+ * @module {{}} author/app_template app_template
+ * @parent api
+ *
+ * This module exposes the author app renderer function and it is needed because
+ * this template is loaded dynamically using `System.import` and a bundle was
+ * required for a production build; since Steal is not able to bundle stacha files
+ * yet, we have to bundle the renderer function instead.
+ *
+ */
+import tpl from './app.stache!';
+
+/**
+ * @function template
+ * Renderer function that returns a document fragment of the author app markup.
+ */
+export let template = tpl;

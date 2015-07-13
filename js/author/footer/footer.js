1a16487e7bd24f2e17e444ef24de449d1f3640d4
Manuel Mujica <evilsaurer@gmail.com>
2015-07-13 17:39:29 -0300

    Create app-footer component.
    Closes #345.

---
+++ js/author/footer/footer.js
@@ -0,0 +1,9 @@
+import Component from 'can/component/';
+import template from './footer.stache!';
+
+import './footer.less!';
+
+Component.extend({
+  template,
+  tag: 'app-footer'
+})
--- js/author/main.js
+++ js/author/main.js
@@ -4,6 +4,7 @@ import loader from '@loader';
 import 'author/styles/';
 import 'author/main.less!';
 
+import './footer/';
 import './templates/';
 import './vertical-navbar/';
 

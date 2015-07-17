bee62f3848753febe4031ef52c84c91aa03db5d1
Manuel Mujica <evilsaurer@gmail.com>
2015-07-17 11:26:20 -0300

    Add documentation to the guide model.
    Closes #358.

--- js/author/models/guide.js
+++ js/author/models/guide.js
@@ -2,6 +2,20 @@ import Model from 'can/model/';
 
 import 'can/map/define/';
 
+/**
+ * @constructor Guide
+ * @inherits can.Model
+ * @parent api-models
+ *
+ * The **Guide** model represents a sample interview or an interview created
+ * by the logged in user.
+ *
+ *  @codestart
+ *    Guide.findAll().then(function(guides) {
+ *    });
+ *  @codeend
+ *
+ */
 let Guide = Model.extend({
   findAll: 'POST CAJA_WS.php',
 
@@ -21,11 +35,19 @@ let Guide = Model.extend({
   }
 }, {
   define: {
+    /**
+     * @property {String} Guide.prototype.title title
+     * @description The guide's title, it defaults to 'Untitled' if not set.
+     */
     title: {
       type: 'string',
       value: 'Untitled'
     },
 
+    /**
+     * @property {Number} Guide.prototype.fileSize fileSize
+     * @description Size of the XMl file that describes the guide, in bytes.
+     */
     fileSize: {
       type: 'number',
       get() {
@@ -33,6 +55,10 @@ let Guide = Model.extend({
       }
     },
 
+    /**
+     * @property {String} Guide.prototype.lastModified lastModified
+     * @description Date when the last modification to the guide happened (e.g '2015-06-22 11:16:36').
+     */
     lastModified: {
       type: 'string',
       get() {
@@ -43,10 +69,20 @@ let Guide = Model.extend({
 });
 
 Guide.List = Guide.List.extend({
+  /**
+   * @function Guide.List.prototype.owned owned
+   * @signature `guides.owned()`
+   * @return {Guide.List} List of guides owned (created) by the logged in user.
+   */
   owned: function() {
     return this.filter(guide => guide.attr('owned'));
   },
 
+  /**
+   * @function Guide.List.prototype.samples samples
+   * @signature `guides.samples()`
+   * @return {Guide.List} List of sample guides stored in the server.
+   */
   samples: function() {
     return this.filter(guide => !guide.attr('owned'));
   }

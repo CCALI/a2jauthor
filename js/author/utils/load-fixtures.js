534e24c9c976b1d9b0f5adea4e18c9886169de8a
Manuel Mujica <evilsaurer@gmail.com>
2015-07-20 11:25:31 -0300

    Move load-fixtures to author/utils folder.
    It also includes some style changes to the module's code, like a missing
    semicolon and some comments to explain what the code is doing.
    
    Closes #375.

--- js/author/utils/load-fixtures.js
+++ js/author/utils/load-fixtures.js
@@ -1,6 +1,10 @@
 var loader = require('@loader');
 var isProduction = loader.env === 'production';
 
+// if this module is loaded, the `import` hook of the available loader will
+// be overridden to make sure the `author/models/fixtures` module is loaded
+// before the app's main module if the environment is not production.
+
 if (!isProduction) {
   // 'import' is a reserved word.
   var _import = loader.import;
@@ -8,10 +12,11 @@ if (!isProduction) {
   loader.import = function(name) {
     if (name === 'author/main') {
       var loader = this;
-      return loader.import('author/models/fixtures/').then(function(){
+      return loader.import('author/models/fixtures/').then(function() {
         return _import.call(loader, name);
       });
     }
+
     return _import.apply(this, arguments);
-  }
+  };
 }

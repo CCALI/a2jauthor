68a651b083d3b82051476daa1c38740672acc7ed
Manuel Mujica <evilsaurer@gmail.com>
2015-09-17 17:33:40 -0300

    Add basic drag and drop to template view.

--- js/author/templates/elements/a2j-template/a2j-template.js
+++ js/author/templates/elements/a2j-template/a2j-template.js
@@ -1,8 +1,11 @@
 import Map from 'can/map/';
+import dragula from 'dragula';
 import Component from 'can/component/';
 import stache from 'can/view/stache/';
 
 import 'can/map/define/';
+import './a2j-template.less!';
+import 'dragula/dist/dragula.css!';
 import 'author/templates/elements/free-form/';
 import 'author/templates/elements/a2j-variable/';
 import 'author/templates/elements/section-title/';
@@ -32,13 +35,16 @@ export default Component.extend({
   tag: 'a2j-template',
   viewModel: A2JTemplateVM,
   events: {
-    'inserted': function($element) {
+    inserted($element) {
       let viewModel = this.viewModel;
 
       viewModel.attr('rootNode').attr('children').map((node) => {
         let renderer = stache(node.attr('component'));
         $element.append(renderer(node.attr('state')));
       });
+
+      // add drag & drop support to the templates view
+      dragula([$element.get(0)]);
     }
   }
 });

c984e0e72a3224cd3125d2b7fdd32243866744f3
Manuel Mujica <evilsaurer@gmail.com>
2015-09-22 16:54:00 -0300

    Allow elements to be duplicated and/or removed.

--- js/author/templates/elements/a2j-template/a2j-template.js
+++ js/author/templates/elements/a2j-template/a2j-template.js
@@ -2,6 +2,7 @@ import Map from 'can/map/';
 import Component from 'can/component/';
 import stache from 'can/view/stache/';
 import dragula from 'dragula/dist/dragula';
+import template from './a2j-template.stache!';
 
 import 'can/map/define/';
 import './a2j-template.less!';
@@ -32,19 +33,43 @@ export let A2JTemplateVM = Map.extend({
 });
 
 export default Component.extend({
+  template,
   tag: 'a2j-template',
   viewModel: A2JTemplateVM,
+
   events: {
-    inserted($element) {
-      let viewModel = this.viewModel;
-
-      viewModel.attr('rootNode').attr('children').map((node) => {
-        let renderer = stache(node.attr('component'));
-        $element.append(renderer(node.attr('state')));
-      });
-
+    inserted($el) {
       // add drag & drop support to the templates view
-      dragula([$element.get(0)]);
+      dragula([$el.get(0)]);
+    },
+
+    'element-toolbar .delete-element click': function($el, evt) {
+      evt.preventDefault();
+
+      let rootNode = this.viewModel.attr('rootNode');
+      let nodeIndex = $el.parents('.node-wrapper').data('node-index');
+
+      rootNode.attr('children').splice(nodeIndex, 1);
+    },
+
+    'element-toolbar .duplicate-element click': function($el, evt) {
+      evt.preventDefault();
+
+      let rootNode = this.viewModel.attr('rootNode');
+      let nodeIndex = $el.parents('.node-wrapper').data('node-index');
+      let originalNode = rootNode.attr('children').attr(nodeIndex);
+      let clonedNode = new Map(originalNode.attr());
+
+      rootNode.attr('children').push(clonedNode);
+    }
+  },
+
+  helpers: {
+    a2jParse(component, state) {
+      state = state.isComputed ?  state() : state;
+      component = component.isComputed ? component() : component;
+
+      return stache(component)(state);
     }
   }
 });

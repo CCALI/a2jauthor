e69c220a1a0ce1adb5331f0f05e99d0bac630bc6
Manuel Mujica <evilsaurer@gmail.com>
2015-09-24 12:05:37 -0300

    Initial setup of CKEditor

--- js/author/templates/elements/free-form/free-form.js
+++ js/author/templates/elements/free-form/free-form.js
@@ -6,6 +6,7 @@ import template from './free-form.stache!';
 import 'can/map/define/';
 import './free-form.less!';
 import '../element-container/';
+import ckeditor from 'ckeditor/';
 
 export let FreeFormVM = Map.extend({
   define: {
@@ -19,6 +20,19 @@ export let FreeFormVM = Map.extend({
 
     editActive: {
       value: false
+    },
+
+    ckeditorInstance: {
+      type: '*'
+    }
+  },
+
+  destroyEditorInstance() {
+    let instance = this.attr('ckeditorInstance');
+
+    if (instance) {
+      instance.destroy();
+      this.attr('ckeditorInstance', null);
     }
   }
 });
@@ -37,6 +51,30 @@ export default Component.extend({
         templateSnippet;
 
       return stache(templateSnippet)();
+    }
+  },
+
+  events: {
+    removed() {
+      this.viewModel.destroyEditorInstance();
+    },
+
+    '{viewModel} editActive': function() {
+      let vm = this.viewModel;
+      let editActive = vm.attr('editActive');
+
+      if (editActive) {
+        // wait for the template to be updated, otherwise the `textarea`
+        // won't be in the DOM when `ckeditor.replace` is called.
+        setTimeout(() => {
+          let $textarea = this.element.find('textarea');
+          let editor = ckeditor.replace($textarea.get(0), {});
+
+          vm.attr('ckeditorInstance', editor);
+        });
+      } else {
+        vm.destroyEditorInstance();
+      }
     }
   }
 });

8e75e68f2a4a8b212ebe2478166fee948150b473
Manuel Mujica <evilsaurer@gmail.com>
2015-10-12 17:07:05 -0300

    Migrate filler control to ES6.

--- js/client/filler/filler.js
+++ js/client/filler/filler.js
@@ -1,32 +1,25 @@
-define(['jquery',
-	'can'],
+import control from 'can/control/';
 
-function($, can) {
+import 'can/view/';
 
-	var Filler = can.Control.extend({
-		init: function() {
-			this._calc();
-		},
+export default Control.extend({
+  init: function() {
+    this._calc();
+  },
 
-		_calc: function() {
-			var offset = this.element.offset(),
-			height = $(window).height() - offset.top;
+  _calc: function() {
+    let offset = this.element.offset();
+    let height = $(window).height() - offset.top;
 
-			this.element.height(height);
-		},
+    this.element.height(height);
+  },
 
-		'{window} resize': function() {
-			this._calc();
-		}
-	});
+  '{window} resize': function() {
+    this._calc();
+  }
+});
 
-	/*
-	Expose an additional html attr for easy access
-	 */
-	can.view.attr('filler', function(el, data) {
-		new Filler(el);
-	});
-
-	return Filler;
-
+// Expose an additional html attr for easy access
+can.view.attr('filler', function(el, data) {
+  new Filler(el);
 });

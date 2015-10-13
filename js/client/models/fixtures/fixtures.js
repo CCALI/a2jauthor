c21e58e547e7ebaf687c9c16fd51092b27885838
Manuel Mujica <evilsaurer@gmail.com>
2015-10-13 10:38:20 -0300

    Move main module and template to root folder.
    Also the main module (along with models/fixtures/) are using ES6 module
    syntax.

--- js/client/models/fixtures/fixtures.js
+++ js/client/models/fixtures/fixtures.js
@@ -1,22 +1,17 @@
-define(['jquery', 'can',
-	'can/util/fixture'
-], function($, can) {
+import fixture from 'can/util/fixture/';
 
-	//can.fixture('GET /interview.json', 'fixtures/real_interview_1.json');
-/*
-	can.fixture('GET /options', function(req, res) {
-		res(200, 'success', '<OPTION VALUE=\"Illinois\">Illinois</OPTION><OPTION VALUE=\"Indiana\">Indiana</OPTION><OPTION VALUE=\"Minnesota\">Minnesota</OPTION><OPTION VALUE=\"Texas\">Texas</OPTION><OPTION VALUE=\"Wyoming\">Wyoming</OPTION>');
-	});
+// fixture('GET /interview.json', 'fixtures/real_interview_1.json');
 
-	can.fixture('POST /autosave.aspx', function(req, res) {
-		res(200, 'success', '');
-	});
+// fixture('GET /options', function(req, res) {
+//   res(200, 'success', '<OPTION VALUE=\"Illinois\">Illinois</OPTION><OPTION VALUE=\"Indiana\">Indiana</OPTION><OPTION VALUE=\"Minnesota\">Minnesota</OPTION><OPTION VALUE=\"Texas\">Texas</OPTION><OPTION VALUE=\"Wyoming\">Wyoming</OPTION>');
+// });
 
-	can.fixture('POST /save.aspx', function(req, res) {
-		res(200, 'success', 'http://google.com');
-	});
+// fixture('POST /autosave.aspx', function(req, res) {
+//   res(200, 'success', '');
+// });
 
-	can.fixture('GET /resume.aspx', 'fixtures/partial.anx');
-*/
+// fixture('POST /save.aspx', function(req, res) {
+//   res(200, 'success', 'http://google.com');
+// });
 
-});
+// fixture('GET /resume.aspx', 'fixtures/partial.anx');

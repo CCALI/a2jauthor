e181fd5507c9884c85b64a79bb5b802b387f638b
Manuel Mujica <evilsaurer@gmail.com>
2015-10-13 08:53:09 -0300

    Migrate a2j-footer to ES6.

--- js/client/footer/footer.js
+++ js/client/footer/footer.js
@@ -1,32 +1,34 @@
-define(['jquery',
-	'can',
-	'text!components/footer/init.stache',
-	'can/view/stache',
-	'jquerypp/dom/cookie'],
-function($, can, init) {
+import $ from 'jquery';
+import Map from 'can/map/';
+import Component from 'can/component/';
+import template from './footer.stache!';
 
-	can.Component.extend({
+import 'jquerypp/dom/cookie/';
 
-		tag: 'a2j-footer',
-		template: can.stache(init),
-		scope: {
-			showDesktop: function() {
-				$.cookie('useDesktop', true, { expires: 1 });
-				window.location = this.attr('mState.desktopURL')+
-				// 2015-02-12 Ensure config passed back to desktop
-				'?'+   $.param({
-					templateURL: this.attr('mState.templateURL'),
-					fileDataURL: this.attr('mState.fileDataURL'),
-					getDataURL: this.attr('mState.getDataURL'),
-					setDataURL: this.attr('mState.setDataURL'),
-					autoSetDataURL: this.attr('mState.autoSetDataURL'),
-					exitURL: this.attr('mState.exitURL'),
-					logURL: this.attr('mState.logURL'),
-					errRepURL: this.attr('mState.errRepURL')
-					});
-			}
-		}
+let FooterVM = Map.extend({
+  showDesktop: function() {
+    $.cookie('useDesktop', true, {expires: 1});
 
-	});
+    let qs = $.param({
+      templateURL: this.attr('mState.templateURL'),
+      fileDataURL: this.attr('mState.fileDataURL'),
+      getDataURL: this.attr('mState.getDataURL'),
+      setDataURL: this.attr('mState.setDataURL'),
+      autoSetDataURL: this.attr('mState.autoSetDataURL'),
+      exitURL: this.attr('mState.exitURL'),
+      logURL: this.attr('mState.logURL'),
+      errRepURL: this.attr('mState.errRepURL')
+    });
 
+    // 2015-02-12 Ensure config passed back to desktop
+    // TODO: Figure out a better way to do this
+    window.location = this.attr('mState.desktopURL') + '?' + qs;
+  }
+});
+
+export default Component.extend({
+  template,
+  leakScope: false,
+  tag: 'a2j-footer',
+  viewModel: FooterVM
 });

8c8d6b62ab3e1b2979fb36944a7bd66d39c02b5a
Manuel Mujica <evilsaurer@gmail.com>
2015-10-13 08:45:44 -0300

    Migrate a2j-intro to ES6.

--- js/client/intro/intro.js
+++ js/client/intro/intro.js
@@ -1,28 +1,27 @@
-define(['can',
-		'text!components/intro/init.stache',
-		'jquerypp/event/swipe',
-		'can/view/stache'
-	],
+import Map from 'can/map/';
+import Component from 'can/component/';
+import template from './intro.stache!';
 
-	function(can, init) {
+import 'jquerypp/event/swipe/';
 
-		can.Component.extend({
-			tag: 'a2j-intro',
-			template: can.stache(init),
-			scope: {
-				navigate: function(button) {
-					this.attr('rState').attr({
-						page: this.attr('interview.firstPage'),
-						view: 'pages'
-					});
-				}
-			},
-			events: {
-				'inserted': function() {
-					this.scope.attr('mState.header', '');
-					this.scope.attr('mState.step', '');
-				}
-			}
-		});
+let IntroVM = Map.extend({
+  navigate: function(button) {
+    this.attr('rState').attr({
+      page: this.attr('interview.firstPage'),
+      view: 'pages'
+    });
+  }
+});
 
-	});
+export default Component.extend({
+  template,
+  tag: 'a2j-intro',
+  viewModel: IntroVM,
+
+  events: {
+    inserted() {
+      this.viewModel.attr('mState.header', '');
+      this.viewModel.attr('mState.step', '');
+    }
+  }
+});

c8c645a1c8d736424ca0e620055174c23c364beb
Manuel Mujica <evilsaurer@gmail.com>
2015-10-13 12:17:29 -0300

    Remove deprecated grunt task (requirejs).

--- js/Gruntfile.js
+++ js/Gruntfile.js
@@ -1,31 +1,6 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-		jsbeautifier: {
-			files: ['client/components/**/*.js',
-				'client/fixtures/**/*.js',
-				'client/models/**/*.js',
-				'client/util/**/*.js'],
-			options: {
-				js: {
-					indentWithTabs: true,
-					indentSize: 2
-				}
-			}
-		},
-
-		requirejs: {
-			compile: {
-				options: {
-					baseUrl: 'client',
-					mainConfigFile: 'client/config.js',
-					include: 'components/app/app',
-					name: 'almond',
-					out: 'client/app.min.js'
-				}
-			}
-		},
-
     clean: {
       build: ['dist/']
     },
@@ -73,24 +48,38 @@ module.exports = function(grunt) {
           }
         }
       }
+    },
+
+    jsbeautifier: {
+      files: ['client/**/*.js'],
+      options: {
+        js: {
+          indentSize: 2,
+          indentChar: " ",
+          indentWithTabs: false
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('testee');
   grunt.loadNpmTasks('documentjs');
   grunt.loadNpmTasks('steal-tools');
+	grunt.loadNpmTasks('grunt-jsbeautifier');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-jsbeautifier');
-	grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   grunt.renameTask('documentjs', 'documentjs-orig');
 
   grunt.registerTask('test', ['testee:firefox']);
-  grunt.registerTask('build', ['clean:build', 'steal-build']);
-  grunt.registerTask('documentjs', ['documentjs-orig', 'copy:icon-font', 'copy:demos', 'less:docs']);
 	grunt.registerTask('beautify', ['jsbeautifier']);
-	grunt.registerTask('default', ['requirejs']);
+  grunt.registerTask('build', ['clean:build', 'steal-build']);
+  grunt.registerTask('documentjs', [
+    'documentjs-orig',
+    'copy:icon-font',
+    'copy:demos',
+    'less:docs'
+  ]);
 
 };

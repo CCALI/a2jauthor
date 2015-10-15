456668019db7f147f004bd5b04bb352d131158ce
Manuel Mujica <evilsaurer@gmail.com>
2015-10-15 10:40:20 -0300

    Add default config values for dev/prod envs.
    Closes #640.

---
+++ js/viewer/config/dev.js
@@ -0,0 +1,28 @@
+export default {
+  // templateURL points to the interview XML. This demo just passes what's on the querystring.
+  // templateURL:'../tests/data/Sample Short Interview Answers.anx?data=something',
+  templateURL: '../tests/data/Sample Short Interview.xml?data=ExtraData4templateURL',
+
+  // fileDataURL: 'images/',
+  fileDataURL:'../tests/data/',
+
+  // getDataURL:'A2J_ViewerExit.html',
+  getDataURL: '../tests/data/Sample Short Interview Answers.anx?data=ExtraData4getDataURL',
+
+  // setDataURL saves answer file and leaves the viewer (its response replaces viewer's frame)
+  setDataURL: '/save.aspx',
+
+  // autoSetDataURL silently saves answer file periodically. (Optional).
+  autoSetDataURL: '/autosave.aspx',
+
+  // exitURL replaces the viewer's frame with this URL on EXIT (user 'fails' interview)
+  exitURL: 'A2J_ViewerExit.html',
+
+  // logURL accepts silent logging data.
+  // E.g., https://lawhelpinteractive.org/a2j_logging
+  logURL: '',
+
+  // errRepURL accepts user's public submission of an error to which they can add an optional comment.
+  // E.g., 'https://lawhelpinteractive.org/problem_reporting_form',
+  errRepURL: ''
+};

05ace2a495505fd601eac7523e687fba8009bfd6
Tom Greever <anybody@tomgreever.com>
2015-11-04 15:00:19 -0600

    Fixing a merge conflict

--- js/styles/styles.js
+++ js/styles/styles.js
@@ -1,8 +1,3 @@
 import 'styles/author.jquery-ui.css!';
 import 'styles/jquery.fileupload-ui.css!';
 import 'styles/A2J_Author.css!';
-
-//import 'styles/viewer/A2J_Fonts.less!';
-//import 'styles/viewer/A2J_Viewer.less!';
-//import 'styles/viewer/jquery-ui.extra.less!';
-//import 'styles/viewer/viewer.jquery-ui.less!';

88d7dd63964bf070c5f85a65b6235056c9dfcf03
Manuel Mujica <evilsaurer@gmail.com>
2015-12-18 19:04:48 -0500

    Fix guide test after fixture updates.

--- js/author/models/tests/guide-test.js
+++ js/author/models/tests/guide-test.js
@@ -5,8 +5,8 @@ import 'steal-mocha';
 
 describe('Guide Model', () => {
 
-  it('retrieves a list of guides properly', () => {
-    return Guide.findAll().then((guides) => {
+  it('retrieves a list of guides properly', function() {
+    return Guide.findAll().then(function(guides) {
       assert(guides.attr('length') > 0);
 
       let guide = guides.attr(0).attr();
@@ -17,22 +17,22 @@ describe('Guide Model', () => {
     });
   });
 
-  it('retrieves list of guides that belong to user', () => {
-    return Guide.findAll().then((guides) => {
+  it('retrieves list of guides that belong to user', function() {
+    return Guide.findAll().then(function(guides) {
       let owned = guides.owned();
 
       assert.isTrue(owned.attr(0).attr('owned'));
-      assert.equal(owned.attr('length'), 1,
-        'there is only 1 owned guide in fixtures');
+      assert.equal(owned.attr('length'), 2,
+        'there are 2 owned guides in the fixtures');
     });
   });
 
-  it('retrieves list of sample guides', () => {
-    return Guide.findAll().then((guides) => {
+  it('retrieves list of sample guides', function() {
+    return Guide.findAll().then(function(guides) {
       let samples = guides.samples();
 
       assert.isFalse(samples.attr(0).attr('owned'));
-      assert.equal(samples.attr('length'), guides.attr('length') - 1);
+      assert.equal(samples.attr('length'), 4);
     });
   });
 

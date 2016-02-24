002b5108dd78c333a6f1a1a2e6a57b6447420893
Manuel Mujica <evilsaurer@gmail.com>
2016-02-24 16:22:10 -0300

    Account for absolute paths and actual file data urls.
    The template's api logic to build the path to look up the json files was
    relying on relative paths from the viewer folder, in production
    environments `fileDataUrl` will probably point to an absolut path or to
    a different server, this commit adds logic to handle those scenarios.

--- src/util/paths.js
+++ src/util/paths.js
@@ -1,6 +1,7 @@
 const Q = require('q');
 const path = require('path');
 const config = require('./config');
+const urlRegex = require('url-regex');
 
 /**
  * @module {Module} /util/paths paths
@@ -14,16 +15,31 @@ module.exports = {
   /**
    * @function paths.getViewerPath
    * @parent paths
-   *
-   * When `fileDataUrl` is used to locate the template(s) json files,
-   * we need to build the templates path using the viewer folder as
-   * the base, since that's the base path the standalone viewer uses
-   * to retrive the Guide.json file needed to render the app.
-   *
-   * @return {String} The path of the viewer app folder
+   * @return {String} Absolute path to the viewer app folder
    */
   getViewerPath() {
     return path.join(__dirname, '..', '..', 'js/viewer');
+  },
+
+  /**
+   * @function paths.normalizeFileDataUrl
+   * @parent paths
+   * @param {String} fileDataUrl URL where the file data folder is located
+   *
+   * When `fileDataUrl` is used to locate the template(s) json files,
+   * we need to build the templates path using the viewer folder as
+   * the base if `fileDataUrl` is relative, otherwise we just use the
+   * absolute `fileDataUrl` as-is.
+   *
+   * @return {String} Normalized file data url
+   */
+  normalizeFileDataUrl(fileDataUrl) {
+    const isAbsolutePath = path.isAbsolute(fileDataUrl);
+    const isUrl = urlRegex({ exact: true }).test(fileDataUrl);
+
+    return (isUrl || isAbsolutePath) ?
+      fileDataUrl :
+      path.join(this.getViewerPath(), fileDataUrl);
   },
 
   /**
@@ -38,7 +54,7 @@ module.exports = {
     const guidesDir = config.get('GUIDES_DIR');
 
     const file = fileDataUrl ?
-      path.join(this.getViewerPath(), fileDataUrl, 'templates.json') :
+      path.join(this.normalizeFileDataUrl(fileDataUrl), 'templates.json') :
       path.join(guidesDir, username, 'templates.json');
 
     deferred.resolve(file);
@@ -62,7 +78,7 @@ module.exports = {
     const filename = `template${templateId}.json`;
 
     const file = fileDataUrl ?
-      path.join(this.getViewerPath(), fileDataUrl, filename) :
+      path.join(this.normalizeFileDataUrl(fileDataUrl), filename) :
       path.join(guidesDir, username, folderName, filename);
 
     deferred.resolve(file);
--- test/util/paths.js
+++ test/util/paths.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const sinon = require('sinon');
 const assert = require('assert');
-const _includes = require('lodash/includes');
 
 const paths = require('../../src/util/paths');
 const config = require('../../src/util/config');
@@ -10,6 +9,10 @@ describe('lib/util/paths', function() {
   let guidesDir;
   let currentUser;
   let configGetStub;
+
+  const projectDirname = function() {
+    return __dirname.substring(0, __dirname.lastIndexOf('CAJA/') + 5);
+  };
 
   beforeEach(function() {
     guidesDir = '/foo/userfiles/';
@@ -25,50 +28,100 @@ describe('lib/util/paths', function() {
   });
 
   describe('getTemplatesPath', function() {
-    it('builds the right path when username provided', function() {
+    it('is correct when username provided', function() {
       const promise = paths.getTemplatesPath({ username: currentUser });
 
-      return promise.then((templatesPath) => {
+      return promise.then(templatesPath => {
         const expected = path.join(guidesDir, currentUser, 'templates.json');
         assert.equal(templatesPath, expected);
       });
     });
 
-    it('builds the right path when fileDataUrl provided', function() {
+    it('is correct when a relative fileDataUrl path is provided', function() {
       const fileDataUrl = 'path/to/file/data';
       const promise = paths.getTemplatesPath({ fileDataUrl });
 
-      return promise.then((templatesPath) => {
-        assert.ok(_includes(templatesPath, `CAJA/js/viewer/${fileDataUrl}`));
+      return promise.then(templatesPath => {
+        const expected = path.join(projectDirname(), `js/viewer/${fileDataUrl}`);
+        assert.ok(templatesPath, expected);
+      });
+    });
+
+    it('is correct when an absolute fileDataUrl path is provided', function() {
+      const fileDataUrl = '/path/to/files';
+      const promise = paths.getTemplatesPath({ fileDataUrl });
+
+      return promise.then(templatesPath => {
+        const expected = path.join(fileDataUrl, 'templates.json');
+        assert.equal(templatesPath, expected, 'absolute fileDataUrl should be used as-is');
+      });
+    });
+
+    it('is correct when an actual fileDataUrl url is provided', function() {
+      const fileDataUrl = 'http://my.awesome.server/path/to/files';
+      const promise = paths.getTemplatesPath({ fileDataUrl });
+
+      return promise.then(templatesPath => {
+        const expected = path.join(fileDataUrl, 'templates.json');
+        assert.equal(templatesPath, expected, 'absolute fileDataUrl should be used as-is');
       });
     });
   });
 
   describe('getTemplatePath', function() {
-    it('builds the right path when username provided', function() {
+    it('is correct when username provided', function() {
       const promise = paths.getTemplatePath({
         guideId: 20,
         templateId: 20,
         username: currentUser
       });
 
-      return promise.then((templatesPath) => {
+      return promise.then(templatePath => {
         const expected = path.join(guidesDir, currentUser, 'guides/Guide20/template20.json');
+        assert.equal(templatePath, expected);
+      });
+    });
+
+    it('is correct when a relative fileDataUrl path is provided', function() {
+      const fileDataUrl = 'path/to/file/data';
+
+      const promise = paths.getTemplatePath({
+        fileDataUrl,
+        templateId: 20,
+      });
+
+      return promise.then(templatePath => {
+        const expected = path.join(projectDirname(), 'js/viewer', fileDataUrl, `template20.json`);
+        assert.equal(templatePath, expected);
+      });
+    });
+
+    it('is correct when an absolute fileDataUrl path is provided', function() {
+      const fileDataUrl = '/path/to/file/data';
+
+      const promise = paths.getTemplatePath({
+        fileDataUrl,
+        templateId: 20,
+      });
+
+      return promise.then(templatesPath => {
+        const expected = path.join(fileDataUrl, 'template20.json');
         assert.equal(templatesPath, expected);
       });
     });
 
-    it('builds the right path when fileDataUrl provided', function() {
+    it('is correct when an actual fileDataUrl url is provided', function() {
+      const fileDataUrl = 'http://my.awesome.server/path/to/files';
+
       const promise = paths.getTemplatePath({
+        fileDataUrl,
         templateId: 20,
-        fileDataUrl: 'path/to/file/data'
       });
 
-      return promise.then((templatesPath) => {
-        const expected = 'CAJA/js/viewer/path/to/file/data/template20.json';
-        assert.ok(_includes(templatesPath, expected));
+      return promise.then(templatesPath => {
+        const expected = path.join(fileDataUrl, 'template20.json');
+        assert.equal(templatesPath, expected);
       });
     });
-
   });
 });

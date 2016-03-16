3cf26761319b7fb0c7044b69c709a74238bc8254
Manuel Mujica <evilsaurer@gmail.com>
2016-03-16 11:01:10 -0300

    Refactor checkbox/radio validation logic.

--- js/viewer/models/tests/answervm-test.js
+++ js/viewer/models/tests/answervm-test.js
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import Field from 'viewer/models/field';
 import Answer from 'viewer/models/answer';
 import Answers from 'viewer/models/answers';
 import AnswerVM from 'viewer/models/answervm';
@@ -50,5 +51,68 @@ describe('AnswerViewModel', function() {
     });
 
     assert.deepEqual(avm.attr('answer.values').attr(), [null, false]);
+  });
+
+  describe('validating checkboxes', function() {
+    let checkboxes;
+
+    beforeEach(function() {
+      const checkboxFactory = function() {
+        const checkbox = new Field({
+          name: 'foo',
+          required: true,
+          type: 'checkbox',
+          repeating: false
+        });
+
+        checkbox.attr('answer', checkbox.attr('emptyAnswer'));
+        return checkbox;
+      };
+
+      checkboxes = new Field.List([
+        checkboxFactory(),
+        checkboxFactory(),
+        checkboxFactory()
+      ]);
+
+      checkboxes.each(function(checkbox) {
+        checkbox.attr('_answer', new AnswerVM({
+          answerIndex: 1,
+          field: checkbox,
+          fields: checkboxes,
+          answer: checkbox.attr('answer')
+        }));
+      });
+    });
+
+    it('fails if no required checkbox has been checked', function() {
+      const checkbox = checkboxes.attr(0);
+
+      // trigger the validation logic
+      checkbox.attr('_answer.values', null);
+
+      assert.isTrue(!!checkbox.attr('_answer').errors(), 'should fail');
+    });
+
+    it('does not fail if a required checkbox has been checked', function() {
+      const checkbox = checkboxes.attr(0);
+
+      // trigger the validation logic
+      checkbox.attr('_answer.values', 'foo');
+
+      assert.isNull(checkbox.attr('_answer').errors(), 'should not fail');
+    });
+
+    it('does not fail if none of the checkboxes is required', function() {
+      checkboxes.each(function(checkbox) {
+        checkbox.attr('required', false);
+      });
+
+      // trigger the validation logic
+      const checkbox = checkboxes.attr(0);
+      checkbox.attr('_answer.values', null);
+
+      assert.isNull(checkbox.attr('_answer').errors(), 'should not fail');
+    });
   });
 });

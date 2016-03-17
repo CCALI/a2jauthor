8367e5951b2aaf9752ec5f6954b6564bad6c9759
Manuel Mujica <evilsaurer@gmail.com>
2016-03-17 10:12:49 -0300

    Fix flaky test.

--- js/viewer/desktop/steps/steps-test.js
+++ js/viewer/desktop/steps/steps-test.js
@@ -1,11 +1,14 @@
 import $ from 'jquery';
+import F from 'funcunit';
 import Map from 'can/map/';
 import assert from 'assert';
+import _round from 'lodash/round';
+import _assign from 'lodash/assign';
 import stache from 'can/view/stache/';
 import AppState from 'viewer/models/app-state';
 import Interview from 'viewer/models/interview';
 import { ViewerStepsVM } from 'viewer/desktop/steps/';
-import _round from 'lodash/round';
+import interviewJSON from 'viewer/models/fixtures/real_interview_1.json';
 
 import 'steal-mocha';
 
@@ -14,43 +17,41 @@ describe('<a2j-viewer-steps>', function() {
     let vm;
     let interview;
 
-    beforeEach(function(done) {
-      let promise = Interview.findOne({url: '/interview.json'});
+    beforeEach(function() {
+      const rawData = _assign({}, interviewJSON);
+      const parsedData = Interview.parseModel(rawData);
 
-      promise.then(function(_interview) {
-        interview = _interview;
+      interview = new Interview(parsedData);
 
-        let mState = new Map();
-        let rState = new AppState({
-          page: interview.attr('firstPage')
-        });
+      const mState = new Map();
+      const page = interview.attr('firstPage');
+      const rState = new AppState({ page });
 
-        let frag = stache(
-          `<a2j-viewer-steps
-            r-state="{rState}"
-            m-state="{mState}"
-            interview="{interview}" />`
-        );
+      const frag = stache(
+        `<a2j-viewer-steps
+          r-state="{rState}"
+          m-state="{mState}"
+          interview="{interview}" />`
+      );
 
-        $('#test-area').html(frag({rState, interview, mState}));
-        vm = $('a2j-viewer-steps').viewModel();
-        done();
-      });
+      $('#test-area').html(frag({rState, interview, mState}));
+      vm = $('a2j-viewer-steps').viewModel();
     });
 
     afterEach(function() {
       $('#test-area').empty();
     });
 
-    it('renders arrow when step number is zero', function() {
-      let firstStepNumber = interview.attr('steps.0.number');
-
+    it('renders arrow when step number is zero', function(done) {
+      const firstStepNumber = interview.attr('steps.0.number');
       assert.equal(parseInt(firstStepNumber, 10), 0);
-      assert.equal($('.glyphicon-step-zero').length, 1, 'should be one arrow');
+
+      F('.glyphicon-step-zero').size(1, 'should be one arrow');
+      F(done);
     });
 
-    it('renders only guide avatar if "userGender" is unknown', function() {
-      let answers = interview.attr('answers');
+    it('renders only guide avatar if "userGender" is unknown', function(done) {
+      const answers = interview.attr('answers');
 
       // user has not set their gender
       answers.attr('user gender', {
@@ -58,21 +59,27 @@ describe('<a2j-viewer-steps>', function() {
         values: [null]
       });
       assert.isUndefined(interview.attr('userGender'));
-      assert.equal($('a2j-viewer-avatar').length, 1);
+
+      F('a2j-viewer-avatar').size(1);
+      F(done);
     });
 
-    it('renders both client and guide avatars if "userGender" is known', function() {
-      let answers = interview.attr('answers');
+    it('renders both client and guide avatars if "userGender" is known',
+      function(done) {
+        const answers = interview.attr('answers');
 
-      // user set her gender.
-      answers.attr('user gender', {
-        name: 'user gender',
-        values: [null, 'f']
-      });
+        // user set her gender.
+        answers.attr('user gender', {
+          name: 'user gender',
+          values: [null, 'f']
+        });
 
-      assert.equal(interview.attr('userGender'), 'female');
-      assert.equal($('a2j-viewer-avatar').length, 2);
-    });
+        assert.equal(interview.attr('userGender'), 'female');
+
+        F('a2j-viewer-avatar').size(2);
+        F(done);
+      }
+    );
   });
 
   describe('ViewModel', function() {

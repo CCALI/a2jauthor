382c47e2ef98ffa7d77b24de17f149400967c393
Elmer Masters <emasters@cali.org>
2016-04-23 14:34:21 -0400

    Update CONFIG.php

--- js/CONFIG.php
+++ js/CONFIG.php
@@ -4,21 +4,21 @@
 	09/07/2013 - 10/05/2012 SJG Customize db and folder paths depending on server.
 */
 
-define("SERVER_URL","http://localhost/");
-define("DRUPAL_ROOT_DIR", "/var/www/drupal-7.43");
-define("GUIDES_DIR","/var/www/CAJA/userfiles");
+define("SERVER_URL","");
+define("DRUPAL_ROOT_DIR", "");
+define("GUIDES_DIR","");
 define("GUIDES_URL", "/userfiles/"); //this shoudldn't change if running at web root
 
 // db variables
-define('DB_NAME', "caja-edev");
-define('DB_USER', "root");
-define('DB_PASSWORD', 'cul8rgys');
-define('DB_HOST', 'localhost');
+define('DB_NAME', "");
+define('DB_USER', "");
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
 
-define('D7_DB_NAME', "d7a2jdev");
-define('D7_DB_USER', "root");
-define('D7_DB_PASSWORD', 'cul8rgys');
-define('D7_DB_HOST', 'localhost');
+define('D7_DB_NAME', "");
+define('D7_DB_USER', "");
+define('D7_DB_PASSWORD', '');
+define('D7_DB_HOST', '');
 
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $drupaldb = new mysqli(D7_DB_HOST,D7_DB_USER,D7_DB_PASSWORD,D7_DB_NAME);

cd9201dfe08f354353ddd1fdd0fda5488c287f44
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2016-10-10 14:55:29 -0300

    Add ability to disable varpicker input

---
+++ js/author/templates/elements/varpicker/tokenfield-disabled-attr.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+import 'can/view/';
+
+can.view.attr('tokenfield-disabled', function(el, attrData) {
+  let $el = $(el);
+  let attrValue = el.getAttribute('tokenfield-disabled');
+  let disabledCompute = attrData.scope.compute(attrValue);
+
+  function onChange(ev, newVal) {
+    $el.tokenfield(newVal ? 'disable' : 'enable');
+  }
+
+  disabledCompute.bind('change', onChange);
+
+  $el.on('tokenfield:destroyed', function() {
+    disabledCompute.unbind(onChange);
+  });
+});

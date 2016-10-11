f2cc4804ff621b01acaba66eecb82fb0349f823a
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2016-10-11 13:03:18 -0300

    Move a2j-variable vm into its own file

---
+++ js/author/templates/elements/a2j-variable/a2j-variable-vm.js
@@ -0,0 +1,88 @@
+import Map from 'can/map/';
+import Answers from 'caja/author/models/answers';
+
+/**
+ * @module {can.Map} A2jTemplateVM
+ * @parent A2JTemplate
+ *
+ * <a2j-variable>'s viewModel.
+ */
+export default Map.extend({
+  define: {
+    /**
+     * @property {Answers} answers
+     *
+     * Answers object available when user uploads an ANX file during document
+     * assembly.
+     */
+    answers: {
+      Type: Answers
+    },
+
+    /**
+     * @property {Answer} variable
+     *
+     * This component has access to the `answers` object, which is a map where
+     * each key is a variable name (lowercase) and it's value is an object with
+     * a few properties, like `name`, `repeating` and `values`. This property
+     * looks up the variable name in `answers` and returns its value if found.
+     */
+    variable: {
+      get() {
+        let name = this.attr('name');
+        let answers = this.attr('answers');
+
+        if (name && answers) {
+          return answers.getVariable(name);
+        }
+      },
+    },
+
+    /**
+     * @property {Number} varIndex
+     *
+     * The index used to access a specific value of a `repeating` variable.
+     *
+     * E.g: if [varIndex] is set to `2`, `variable.attr('values.2')` will be
+     * returned if `variable` is `repeating`.
+     */
+    varIndex: {
+      value: null
+    },
+
+    /**
+     * @property {String} value
+     *
+     * The effective variable value.
+     */
+    value: {
+      get() {
+        let variable = this.attr('variable');
+
+        if (variable) {
+          let name = this.attr('name');
+          let answers = this.attr('answers');
+          let index = this.attr('varIndex');
+
+          return answers.getValue(name, index);
+        }
+      }
+    },
+
+    /**
+     * @property {Boolean} showValue
+     *
+     * Whether to show the variable value in the answers object instead of the
+     * variable name pass to the component through its attributes.
+     */
+    showValue: {
+      get() {
+        let value = this.attr('value');
+        let useAnswers = this.attr('useAnswers');
+
+        return value != null && useAnswers;
+      }
+    }
+  }
+});
+
--- js/author/templates/elements/a2j-variable/a2j-variable.js
+++ js/author/templates/elements/a2j-variable/a2j-variable.js
@@ -1,13 +1,10 @@
-import Map from 'can/map/';
 import _assign from 'lodash/assign';
 import Component from 'can/component/';
-import template from './a2j-variable.stache!';
-import Answers from 'caja/author/models/answers';
-
-import 'can/map/define/';
+import template from './a2j-variable.stache';
+import A2JVariableVM from './a2j-variable-vm';
 
 /**
- * @module {Module} author/templates/elements/a2j-variable/ <a2j-variable>
+ * @module {Module} A2jVariable
  * @parent api-components
  *
  * This component represents a variable of an interview; it renders a small
@@ -21,97 +18,6 @@ import 'can/map/define/';
  *   <a2j-variable name="Client First Name" />
  * @codeend
  */
-
-/**
- * @property {can.Map} variable.ViewModel
- * @parent author/templates/elements/a2j-variable/
- *
- * <a2j-variable>'s viewModel.
- */
-export let A2JVariableVM = Map.extend({
-  define: {
-    /**
-     * @property {Answers} variable.ViewModel.prototype.answers answers
-     * @parent variable.ViewModel
-     *
-     * Answers object available when user uploads an ANX file during document
-     * assembly.
-     */
-    answers: {
-      Type: Answers
-    },
-
-    /**
-     * @property {Answer} variable.ViewModel.prototype.variable variable
-     * @parent variable.ViewModel
-     *
-     * This component has access to the `answers` object, which is a map where
-     * each key is a variable name (lowercase) and it's value is an object with
-     * a few properties, like `name`, `repeating` and `values`. This property
-     * looks up the variable name in `answers` and returns its value if found.
-     */
-    variable: {
-      get() {
-        let name = this.attr('name');
-        let answers = this.attr('answers');
-
-        if (name && answers) {
-          return answers.getVariable(name);
-        }
-      },
-    },
-
-    /**
-     * @property {Number} variable.ViewModel.prototype.varIndex varIndex
-     * @parent variable.ViewModel
-     *
-     * The index used to access a specific value of a `repeating` variable.
-     *
-     * E.g: if [varIndex] is set to `2`, `variable.attr('values.2')` will be
-     * returned if `variable` is `repeating`.
-     */
-    varIndex: {
-      value: null
-    },
-
-    /**
-     * @property {String} variable.ViewModel.prototype.value value
-     * @parent variable.ViewModel
-     *
-     * The effective variable value.
-     */
-    value: {
-      get() {
-        let variable = this.attr('variable');
-
-        if (variable) {
-          let name = this.attr('name');
-          let answers = this.attr('answers');
-          let index = this.attr('varIndex');
-
-          return answers.getValue(name, index);
-        }
-      }
-    },
-
-    /**
-     * @property {Boolean} variable.ViewModel.prototype.showValue showValue
-     * @parent variable.ViewModel
-     *
-     * Whether to show the variable value in the answers object instead of the
-     * variable name pass to the component through its attributes.
-     */
-    showValue: {
-      get() {
-        let value = this.attr('value');
-        let useAnswers = this.attr('useAnswers');
-
-        return value != null && useAnswers;
-      }
-    }
-  }
-});
-
 export default Component.extend({
   template,
   tag: 'a2j-variable',

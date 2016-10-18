a93dd3f40c276ffe5432389c0bc14f1196e2177f
Manuel Mujica <evilsaurer@gmail.com>
2016-10-18 10:04:51 -0300

    Revert "Fix A2J SUM function returning only 0"

--- js/viewer/mobile/util/tests/tests.js
+++ js/viewer/mobile/util/tests/tests.js
@@ -2,4 +2,3 @@ import 'viewer/mobile/util/tests/lang-test';
 import 'viewer/mobile/util/tests/logic-test';
 import 'viewer/mobile/util/tests/parser-test';
 import 'viewer/mobile/util/tests/validations-test';
-import 'viewer/mobile/util/tests/tlogic-test';

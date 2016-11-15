d90a53f2ff92355b589df4457461a822c2dfa662
Mike Mitchel <mike@mikemitchel.com>
2016-11-15 10:33:05 -0600

    Fix Steps starting on one instead of Zero
    In the Steps tab while in A2J Author, the first step was being reset
    to one instead of zero.  The problem was in the way A2J_Tabs.js was
    preparing the DOM for view which was actually overwriting the current
    Step values.  Also set MAXSTEPS to 13 as it was incorrectly set to
    12.
    
    Closes # 1426

--- js/author/src/viewer/A2J_Types.js
+++ js/author/src/viewer/A2J_Types.js
@@ -82,7 +82,7 @@ var CONST = {
 	MAXFIELDS: 9,
 	// Reasonable limit on buttons per question
 	MAXBUTTONS: 3,
-	MAXSTEPS: 12,
+	MAXSTEPS: 13,
 	kMinYear: 1900,
 
 	// 11/27/07 1.7.7 Ordering options for lists such as a county list

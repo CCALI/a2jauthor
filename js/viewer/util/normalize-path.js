6fa46a7f928f2be463f39e5e135c4a0c18ea889d
Mike Mitchel <mike@mikemitchel.com>
2016-12-20 13:19:46 -0600

    Fix fully qualified setDataURL doubling domain
    LHI had issue where references to assets like images, video
    and audio were making GET requests to malformed paths.
    setDataURL now works with fully quailfied paths. Updated comments
    to document requirement for trailing slash on setDataURL.

--- js/viewer/util/normalize-path.js
+++ js/viewer/util/normalize-path.js
@@ -12,6 +12,11 @@ export default function(pathToGuideFolder, filePath) {
   // handle paths using either slash or backslash (windows) as a separator
   let filename = filePath.split('\\').pop().split('/').pop();
 
+  // GuideFolder can also be fully qualified, assumes trailing slash
+  if (pathToGuideFolder.indexOf('http') === 0) {
+    return pathToGuideFolder + filename;
+  }
+
   // ...otherwise default to file within guide's folder
   return path.join(pathToGuideFolder, filename);
 }

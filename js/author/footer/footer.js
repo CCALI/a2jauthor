a102b8b8c2f7f000c6d778fbaa7956f29dcc6f34
Mike Mitchel <mike@mikemitchel.com>
2017-02-14 15:58:06 -0600

    Add version number to footer
    Added A2J version number and date to app footer.
    
    Closes #1297

--- js/author/footer/footer.js
+++ js/author/footer/footer.js
@@ -1,8 +1,21 @@
 import Component from 'can/component/';
 import template from './footer.stache!';
+import Map from 'can/map/';
+import constants from 'viewer/models/constants';
+
+let FooterVM = Map.extend({
+  define: {
+    viewerVersion: {
+      get() {
+        return 'A2J ' + constants.A2JVersionNum + '-' + constants.A2JVersionDate;
+      }
+    }
+  }
+});
 
 export default Component.extend({
   template,
+  viewModel: FooterVM,
   leakScope: false,
   tag: 'app-footer'
 });

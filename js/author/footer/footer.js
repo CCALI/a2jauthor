e83537a3af21837e8d2f9a506889d74e653da271
Mike Mitchel <mike@mikemitchel.com>
2017-02-15 13:00:50 -0600

    Added currentYear prop to populate copyright year

--- js/author/footer/footer.js
+++ js/author/footer/footer.js
@@ -2,12 +2,18 @@ import Component from 'can/component/';
 import template from './footer.stache!';
 import Map from 'can/map/';
 import constants from 'viewer/models/constants';
+import moment from 'moment';
 
 let FooterVM = Map.extend({
   define: {
     viewerVersion: {
       get() {
         return 'A2J ' + constants.A2JVersionNum + '-' + constants.A2JVersionDate;
+      }
+    },
+    currentYear: {
+      get() {
+          return moment().year();
       }
     }
   }

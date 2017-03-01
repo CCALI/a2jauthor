b5c0498ac357127c4ceddf1a57edd3610933e90c
Obaid Ahmed <obaid@bitovi.com>
2017-03-01 01:03:02 -0500

    Refactored code to ensure that values are saved when navigating. Also added the 'name' attribute to all fields

--- js/viewer/mobile/pages/pages-vm.js
+++ js/viewer/mobile/pages/pages-vm.js
@@ -191,7 +191,33 @@ export default Map.extend({
     });
   },
 
+  handleIE11(page) {
+    if(!!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+      //only do this if user is using IE11
+      //this is to handle the mis-firing of `change` event
+      //in IE11 when "tabbing" through the fields
+
+      const fields = page.attr('fields');
+      const logic = this.attr('logic');
+
+      let answerIndex = this.attr("rState.i") ? this.attr("rState.i") : 1;
+      let answers = logic.attr("interview.answers");
+
+      fields.each(function(field){
+        let type = field.attr("type");
+        let val = $("input[id='"+ field.attr("label")+"']").val();
+        if(type == "gender") {
+          val = $("input[name='gender']:checked").val();
+        }
+        if(answers) {
+          answers.attr(field.attr("name").toLowerCase()).attr("values." + answerIndex, val);
+        }
+      });
+    }
+  },
+
   navigate(button) {
+
     // Author preview should not post to server
     let previewActive = this.attr('rState').attr('previewActive');
     //
@@ -236,6 +262,7 @@ export default Map.extend({
     const page = this.attr('currentPage');
     const fields = page.attr('fields');
 
+    this.handleIE11(page);
     this.traceButtonClicked(button.attr('label'));
 
     // Set answers for buttons with values

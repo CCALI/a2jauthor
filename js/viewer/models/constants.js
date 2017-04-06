af69fea197e9dc8a3a4a396c90e249e2d36e138e
Mike Mitchel <mike@mikemitchel.com>
2017-04-06 09:35:00 -0500

    Add upload to LHI production button
    Allow Authors to publish directly to LHI's produciton version of the
    viewer via the Publish tab.
    
    Closes 1591

--- js/viewer/models/constants.js
+++ js/viewer/models/constants.js
@@ -7,7 +7,7 @@ export default {
   AJAXLoader: '<span class="loader">&nbsp;</span>',
 
   A2JVersionNum: '6.0', //VersionInfo.verNum
-  A2JVersionDate: '2017-03-20',
+  A2JVersionDate: '2017-04-06',
 
   //CAVersionNum:'5.0.0',
   //CAVersionDate:'2013-04-15',

edef5fb75dd7af9ec3e72186997061b6fbbdd328
Mike Mitchel <mike@mikemitchel.com>
2017-04-21 14:28:43 -0500

    1376 - Add expression evaluation
    Author debug panel now has working input for A2J scripting expressions
    to be live tested by Authors. It takes either primitive values or will
    resolve the values of current variables, and then post the expression
    result to the trace log panel. Expression error messages also show in
    trace log panel if the Author makes a typo or syntax error.
    
    Closes 1376

---
+++ js/author/debug-panel/evaluate/evaluate.js
@@ -0,0 +1,66 @@
+import Map from 'can/map/';
+import Component from 'can/component/';
+import template from './evaluate.stache!';
+
+import 'can/map/define/';
+
+/**
+ * @property {can.Map} evaluatePanel.ViewModel
+ * @parent <author-evaluate-panel>
+ *
+ * `<author-evaluate-panel>`'s viewModel.
+ */
+export let EvaluatePanelVM = Map.extend('EvaluatePanelVM',{
+    define: {
+        traceLogic: {
+            value: null
+        }
+    },
+
+    /**
+     * @function authorEvaluatePanel.ViewModel.prototype.evaluateScript evaluateScript
+     * @parent authorEvaluatePanel.ViewModel
+     *
+     * fire A2J script expression
+     *
+     * Allows the Author to do live testing of expressions based on supplied values or
+     * current values for variables in the interview.
+     *
+     * ex: [User Gender] = "Female" or DOLLARROUND(54332.64)
+     */
+    evaluateScript() {
+        let evalResults;
+        let traceLogic = this.attr('traceLogic');
+        let scriptText = $('#evaluate-input').val();
+        if (scriptText) {
+            evalResults = window.gLogic.evalBlock(scriptText);
+        }
+
+        if (evalResults.errors.length > 0) {
+            evalResults.errors.forEach(function(error) {
+                traceLogic.push({
+                    expression: [ { format: 'valF', msg: 'ERROR' }, { format: 'code', msg: error.text } ]
+                });
+            });
+        } else if (evalResults.text &&  traceLogic) {
+            traceLogic.push({
+                expression: [ { format: 'code', msg: scriptText + ' evaluates to: ' }, { format: 'val', msg: evalResults.text } ]
+            });
+        }
+    }
+});
+
+export default Component.extend({
+  template,
+  leakScope: false,
+  viewModel: EvaluatePanelVM,
+  tag: 'author-evaluate-panel',
+
+  events: {
+    '#evaluate-input keyup': function(el, ev) {
+        if (ev.keyCode===13) {
+            $('#evaluate-button').click();
+        }
+    },
+  }
+});

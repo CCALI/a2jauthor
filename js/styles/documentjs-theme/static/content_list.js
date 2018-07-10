40fb8f1c5abfa4616dae6f363a50348a4aa96f97
Mike Mitchel <mike@mikemitchel.com>
2018-07-10 10:07:29 -0500

    restore document-js theme files

--- js/styles/documentjs-theme/static/content_list.js
+++ js/styles/documentjs-theme/static/content_list.js
@@ -15,46 +15,46 @@ steal("can/control","jquery","can/observe",function(Control, $){
 	return can.Control.extend({
 		init: function() {
 			var sections = [];
-	
+
 			this.collectSignatures().each(function(ix) {
 				var h2 = $('h2', this);
 				this.id = 'sig_' + h2.text().replace(/\s/g,"").replace(/[^\w]/g,"_");
 				//this.id = encodeURIComponent(h2.text());
 				sections.push({id: this.id, text: h2.text()});
 			});
-			
+
 			var headingStack = [],
 				last = function(){
 					return headingStack[ headingStack.length -1 ]
 				};
-			
+
 			var ch = this.collectHeadings().each(function(ix) {
 				var el = $(this);
 				this.id = 'section_' + el.text().replace(/\s/g,"").replace(/[^\w]/g,"_");
 				var num = +this.nodeName.substr(1);
 				var section = {
-					id: this.id, 
+					id: this.id,
 					text: el.text(),
 					num: num,
 					sections: []
 				};
-				
+
 				while(last() && (last().num >= num) ) {
 					headingStack.pop();
 				}
-				
+
 				if(!headingStack.length) {
 					sections.push(section);
 					headingStack.push(section);
 				} else {
 					last().sections.push(section);
 					headingStack.push(section);
-				}	
+				}
 			});
-	
-			this.element.html( contentList(sections, 
+
+			$(this.element).html( contentList(sections,
 				( ( window.docObject.outline && window.docObject.outline.tag ) || "ul" ).toLowerCase() ) );
-	
+
 			if(window.location.hash.length) {
 				var id = window.location.hash.replace('#', ''),
 					anchor = document.getElementById(id);

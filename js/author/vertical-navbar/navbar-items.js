eea88522110393ec4acc97efd570cb34c09fec2d
Mike Mitchel <mike@mikemitchel.com>
2018-07-24 15:08:35 -0500

    explicitly set active to false

--- js/author/vertical-navbar/navbar-items.js
+++ js/author/vertical-navbar/navbar-items.js
@@ -3,73 +3,85 @@ export default [
     page: 'about',
     title: 'About',
     ref: 'tabsAbout',
-    icon: 'glyphicon-info-circled'
+    icon: 'glyphicon-info-circled',
+    active: false
   },
   {
     page: 'variables',
     title: 'Variables',
     ref: 'tabsVariables',
-    icon: 'glyphicon-variables'
+    icon: 'glyphicon-variables',
+    active: false
   },
   {
     page: 'steps',
     title: 'Steps',
     ref: 'tabsSteps',
-    icon: 'glyphicon-list-numbered'
+    icon: 'glyphicon-list-numbered',
+    active: false
   },
   {
     page: 'pages',
     title: 'Pages',
     ref: 'tabsPages',
-    icon: 'glyphicon-docs'
+    icon: 'glyphicon-docs',
+    active: false
   },
   {
     page: 'map',
     title: 'Map',
     ref: 'tabsMap',
-    icon: 'glyphicon-sitemap'
+    icon: 'glyphicon-sitemap',
+    active: false
   },
   {
     page: 'files',
     title: 'Files',
     ref: 'tabsFiles',
-    icon: 'glyphicon-attach'
+    icon: 'glyphicon-attach',
+    active: false
   },
   {
     page: 'allLogic',
     title: 'All Logic',
     ref: 'tabsLogic',
-    icon: 'glyphicon-split'
+    icon: 'glyphicon-split',
+    active: false
   },
   {
     page: 'allText',
     title: 'All Text',
     ref: 'tabsText',
-    icon: 'glyphicon-pencil'
+    icon: 'glyphicon-pencil',
+    active: false
   },
   {
     page: 'preview',
     title: 'Preview',
     ref: 'tabsPreview',
-    icon: 'glyphicon-eye'
+    icon: 'glyphicon-eye',
+    active: false
   },
   {
     page: 'report',
     title: 'Report',
     ref: 'tabsReport',
-    icon: 'glyphicon-clipboard'
+    icon: 'glyphicon-clipboard',
+    active: false
   },
   {
     page: 'publish',
     title: 'Publish',
     ref: 'tabsUpload',
-    icon: 'glyphicon-publish'
+    icon: 'glyphicon-publish',
+    active: false
   },
   {
     page: 'templates',
     title: 'Templates',
     ref: 'tabsTemplate',
-    icon: 'glyphicon-doc-text'
+    icon: 'glyphicon-doc-text',
+    active: false
   },
   {
     page: 'interviews',

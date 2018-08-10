3935da3cfd6d88260f596f433e7acec7d9288b13
Mike Mitchel <mike@mikemitchel.com>
2018-08-10 14:53:37 -0500

    set lang props on init

--- js/viewer/mobile/util/lang.js
+++ js/viewer/mobile/util/lang.js
@@ -3,10 +3,13 @@ import TLang from 'caja/viewer/mobile/util/tlang'
 import cString from 'caja/viewer/mobile/util/string'
 
 export default DefineMap.extend({
-  setup: function (id) {
+  seal: false
+},
+{
+  init: function (id) {
     var o = {}
 
     TLang(o, cString.makestr.bind(cString)).set(id)
-    return DefineMap.prototype.setup.call(this, o)
+    return this.assign(o)
   }
 })

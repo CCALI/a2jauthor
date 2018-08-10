9812dd82924fa0c273e0b434da592c1323f4c925
Mike Mitchel <mike@mikemitchel.com>
2018-08-10 10:11:35 -0500

    update lang to definemap

--- js/viewer/mobile/util/lang.js
+++ js/viewer/mobile/util/lang.js
@@ -1,12 +1,12 @@
-import CanMap from 'can-map';
+import DefineMap from 'can-define/map/map'
 import TLang from 'caja/viewer/mobile/util/tlang'
 import cString from 'caja/viewer/mobile/util/string'
 
-export default CanMap.extend({
-  init: function (id) {
+export default DefineMap.extend({
+  setup: function (id) {
     var o = {}
 
     TLang(o, cString.makestr.bind(cString)).set(id)
-    this.attr(o)
+    return DefineMap.prototype.setup.call(this, o)
   }
 })

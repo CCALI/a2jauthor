d40936373e81368442bb1e707bc469d74bb4c850
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2018-12-19 17:23:47 -0600

    update fixtures to use key of id instead of guid

--- js/author/models/fixtures/legal-nav-resources.js
+++ js/author/models/fixtures/legal-nav-resources.js
@@ -1,21 +1,21 @@
 import fixture from 'can-fixture'
 
-export default fixture('GET /api/topics-resources/resource', function (req, res) {
+export default fixture('GET /api/topics-resources/resources', function (req, res) {
   const topic = req.data ? req.data.topic : null
 
   const divorceResources = [
-    {name: 'How to file uncontested divorce', guid: '1q2w3e4r'},
-    {name: 'Dividing properties and vehicles', guid: '5t6y7u8i'}
+    {name: 'How to file uncontested divorce', id: '1q2w3e4r'},
+    {name: 'Dividing properties and vehicles', id: '5t6y7u8i'}
   ]
 
   const mooseResources = [
-    {name: 'Do not approach a wild moose!', guid: 'p1p3p5p7'},
-    {name: 'Moose and the art of leaving them alone', guid: '0f9cv7sd'}
+    {name: 'Do not approach a wild moose!', id: 'p1p3p5p7'},
+    {name: 'Moose and the art of leaving them alone', id: '0f9cv7sd'}
   ]
 
   const surfingResources = [
-    {name: 'Gnarly waves and how to navigate them', guid: 'n6kd8vj4'},
-    {name: 'Staying in the Green Room', guid: 'j69s65jb3'}
+    {name: 'Gnarly waves and how to navigate them', id: 'n6kd8vj4'},
+    {name: 'Staying in the Green Room', id: 'j69s65jb3'}
   ]
 
   switch (topic) {

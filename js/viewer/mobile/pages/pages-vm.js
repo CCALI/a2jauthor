cedc91d973f2608aa381e570d9b6c6e936745443
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2019-10-25 21:23:27 -0500

    break navigate into funciton call steps

--- js/viewer/mobile/pages/pages-vm.js
+++ js/viewer/mobile/pages/pages-vm.js
@@ -177,6 +177,8 @@ export default CanMap.extend('PagesVM', {
       const hasError = !!field.attr('_answer.errors')
       field.attr('hasError', hasError)
     })
+
+    return _some(fields, f => f.attr('hasError'))
   },
 
   traceButtonClicked (buttonLabel) {
@@ -201,160 +203,178 @@ export default CanMap.extend('PagesVM', {
     vm.traceButtonClicked(button.attr('label'))
 
     const page = vm.attr('currentPage')
-    const fields = page.attr('fields')
+    const anyFieldHasError = vm.validateAllFields()
 
-    vm.validateAllFields()
-    const anyFieldWithError = _some(fields, f => f.attr('hasError'))
     // do nothing if there are field(s) with error(s) - prevent form submission events
-    if (anyFieldWithError) {
+    if (anyFieldHasError) {
       ev && ev.preventDefault()
       return false
-    } else {
-      // no errors/normal navigation
+    } else { // no errors/normal navigation
       const rState = vm.attr('rState')
       const logic = vm.attr('logic')
 
       // Author Preview Mode changes handling of special buttons, and does not post answers
       if (rState.previewActive && vm.hasSpecialButton(button)) {
-        // stop default submit actions in preview
         ev && ev.preventDefault()
-        vm.previewActiveResponses(button)
+        vm.previewActiveResponses(button, ev)
       }
 
-      // resumeInterview function passed from NavigationVM via stache
+      // RESUME button skips rest of navigate
       if (button.next === constants.qIDRESUME) {
         vm.resumeInterview()
-        // special destination dIDRESUME button skips rest of navigate
         return
       }
 
       // special destination qIDFAIL button skips rest of navigate
       // Author can provide an external URL to explain why user did not qualify
       if (button.next === constants.qIDFAIL) {
-        vm.setInterviewAsComplete()
-        let failURL = button.url.toLowerCase()
-        let hasProtocol = failURL.indexOf('http') === 0
-        failURL = hasProtocol ? failURL : 'http://' + failURL
-        if (failURL === 'http://') {
-          // If Empty, standard message
-          vm.attr('modalContent', {
-            title: 'You did not Qualify',
-            text: 'Unfortunately, you did not qualify to use this A2J Guided Interview. Please close your browser window or tab to exit the interview.'
-          })
-        } else {
-          // track the external link
-          if (window._paq) {
-            Analytics.trackExitLink(failURL, 'link')
-          }
-          window.open(failURL, '_blank')
-        }
+        vm.handleFailButton(button, vm)
         return
       }
 
       // Set answers for buttons with values
       if (button.name) {
-        const buttonAnswer = vm.__ensureFieldAnswer(button)
-        let buttonAnswerIndex = 1
-
-        if (page.attr('repeatVar')) {
-          const repeatVar = page.attr('repeatVar')
-          const repeatVarCount = logic.varGet(repeatVar)
-
-          buttonAnswerIndex = (repeatVarCount != null) ? repeatVarCount : buttonAnswerIndex
-        }
-
-        let buttonValue = button.value
-
-        if (buttonAnswer.type === 'TF') {
-          buttonValue = buttonValue.toLowerCase() === 'true'
-        } else if (buttonAnswer.type === 'Number') {
-          buttonValue = parseInt(buttonValue)
-        }
-
-        buttonAnswer.attr('values.' + buttonAnswerIndex, buttonValue)
+        vm.handleButtonValue(button, vm, page, logic)
       }
 
       // handle afterLogic
-      const codeAfter = page.attr('codeAfter')
-      // default next page is derived from the button pressed.
-      // might be overridden by the After logic or special
-      // back to prior question button.
-      logic.attr('gotoPage', button.next)
-
-      // execute After logic only if not going to a prior question
-      if (codeAfter && button.next !== constants.qIDBACK) {
-        vm.traceMessageAfterQuestion()
-
-        // parsing codeAfter causes several re-renders, batching for performance
-        // TODO: might not need this once afterLogic parsing is refactored to canjs
-        queues.batch.start()
-        logic.exec(codeAfter)
-        queues.batch.stop()
-      }
+      vm.handleCodeAfter(button, page, logic, vm)
 
       // Don't post to the server in Author Preview aka previewActive
-      if (!rState.previewActive && (button.next === constants.qIDASSEMBLESUCCESS || button.next === constants.qIDSUCCESS || button.next === constants.qIDEXIT)) {
-        // This modal and disable is for LHI/HotDocs issue taking too long to process
-        // prompting users to repeatedly press submit, crashing HotDocs
-        // Matches A2J4 functionality, but should really be handled better on LHI's server
-        vm.attr('modalContent', {
-          title: 'Answers Submitted :',
-          text: 'Page will redirect shortly'
-        })
-
-        vm.dispatch('post-answers-to-server')
-
-        // qIDASSEMBLESUCCESS requires the default event to trigger the assemble post
-        // and the manual submit below to trigger the answer save
-        // TODO: the way final answer forms are created and submitted needs a refactor
-        if (button.next !== constants.qIDASSEMBLESUCCESS) {
-          ev && ev.preventDefault()
-        }
-        vm.setInterviewAsComplete()
-
-        // disable the previously clicked button
-        setTimeout(() => {
-          $('button:contains(' + button.label + ')').prop('disabled', true)
-        })
+      if (!rState.previewActive && (vm.shouldPostOrAssemble(button))) {
+        vm.handleServerPost(rState, button, vm, ev)
       }
 
       // user has selected to navigate to a prior question
       if (button.next === constants.qIDBACK) {
-        // last visited page always at index 1
-        const priorQuestionName = rState.visitedPages[1].name
-        // override with new gotoPage
-        logic.attr('gotoPage', priorQuestionName)
-        button.attr('next', priorQuestionName)
+        vm.handleBackButton(rState, button, logic)
       }
 
-      const gotoPage = logic.attr('gotoPage')
-      const logicPageIsNotEmpty = _isString(gotoPage) && gotoPage.length
-
-      // this means the logic After has overridden the destination page, we
-      // should navigate to this page instead of the page set by `button.next`.
-      if (logicPageIsNotEmpty && gotoPage !== button.next) {
-        logic.attr('gotoPage', null)
-        rState.page = gotoPage
-
-      // only navigate to the `button.next` page if the button clicked is not
-      // any of the buttons with "special" behavior.
-      } else if (!vm.hasSpecialButton(button)) {
-        // first update any repeatVars set by buttons, setting to or incrementing by 1
-        const buttonRepeatVar = button.attr('repeatVar')
-        const buttonRepeatVarSet = button.attr('repeatVarSet')
-        if (buttonRepeatVar && buttonRepeatVarSet) {
-          vm.setRepeatVariable(buttonRepeatVar, buttonRepeatVarSet)
-        }
-
-        // nav to next page
-        rState.page = button.next
-      }
-
+      vm.handleGotoOrNormalNav(rState, button, logic, vm)
       // if these special buttons are used, the interview is complete (incomplete is false)
       if (vm.shouldCompleteInterview(button)) {
         vm.setInterviewAsComplete()
       }
     }
+  },
+
+  handleGotoOrNormalNav (rState, button, logic, vm) {
+    const gotoPage = logic.attr('gotoPage')
+    const logicPageIsNotEmpty = _isString(gotoPage) && gotoPage.length
+
+    // this means the logic After has overridden the destination page, we
+    // should navigate to this page instead of the page set by `button.next`.
+    if (logicPageIsNotEmpty && gotoPage !== button.next) {
+      logic.attr('gotoPage', null)
+      rState.page = gotoPage
+
+    // only navigate to the `button.next` page if the button clicked is not
+    // any of the buttons with "special" behavior.
+    } else if (!vm.hasSpecialButton(button)) {
+      // first update any repeatVars set by buttons, setting to or incrementing by 1
+      const buttonRepeatVar = button.attr('repeatVar')
+      const buttonRepeatVarSet = button.attr('repeatVarSet')
+      if (buttonRepeatVar && buttonRepeatVarSet) {
+        vm.setRepeatVariable(buttonRepeatVar, buttonRepeatVarSet)
+      }
+
+      // nav to next page
+      rState.page = button.next
+    }
+  },
+
+  handleFailButton (button, vm) {
+    vm.setInterviewAsComplete()
+    let failURL = button.url.toLowerCase()
+    let hasProtocol = failURL.indexOf('http') === 0
+    failURL = hasProtocol ? failURL : 'http://' + failURL
+    if (failURL === 'http://') {
+      // If Empty, standard message
+      vm.attr('modalContent', {
+        title: 'You did not Qualify',
+        text: 'Unfortunately, you did not qualify to use this A2J Guided Interview. Please close your browser window or tab to exit the interview.'
+      })
+    } else {
+      // track the external link
+      if (window._paq) {
+        Analytics.trackExitLink(failURL, 'link')
+      }
+      window.open(failURL, '_blank')
+    }
+  },
+
+  handleButtonValue (button, vm, page, logic) {
+    const buttonAnswer = vm.__ensureFieldAnswer(button)
+    let buttonAnswerIndex = 1
+
+    if (page.attr('repeatVar')) {
+      const repeatVar = page.attr('repeatVar')
+      const repeatVarCount = logic.varGet(repeatVar)
+
+      buttonAnswerIndex = (repeatVarCount != null) ? repeatVarCount : buttonAnswerIndex
+    }
+
+    let buttonValue = button.value
+
+    if (buttonAnswer.type === 'TF') {
+      buttonValue = buttonValue.toLowerCase() === 'true'
+    } else if (buttonAnswer.type === 'Number') {
+      buttonValue = parseInt(buttonValue)
+    }
+
+    buttonAnswer.attr('values.' + buttonAnswerIndex, buttonValue)
+  },
+
+  handleCodeAfter (button, page, logic, vm) {
+    const codeAfter = page.attr('codeAfter')
+    // default next page is derived from the button pressed.
+    // might be overridden by the After logic or special
+    // back to prior question button.
+    logic.attr('gotoPage', button.next)
+
+    // execute After logic only if not going to a prior question
+    if (codeAfter && button.next !== constants.qIDBACK) {
+      vm.traceMessageAfterQuestion()
+
+      // parsing codeAfter causes several re-renders, batching for performance
+      // TODO: might not need this once afterLogic parsing is refactored to canjs
+      queues.batch.start()
+      logic.exec(codeAfter)
+      queues.batch.stop()
+    }
+  },
+
+  handleServerPost (rState, button, vm, ev) {
+    // This modal and disable is for LHI/HotDocs issue taking too long to process
+    // prompting users to repeatedly press submit, crashing HotDocs
+    // Matches A2J4 functionality, but should really be handled better on LHI's server
+    vm.attr('modalContent', {
+      title: 'Answers Submitted :',
+      text: 'Page will redirect shortly'
+    })
+
+    vm.dispatch('post-answers-to-server')
+
+    // qIDASSEMBLESUCCESS requires the default event to trigger the assemble post
+    // and the manual submit below to trigger the answer save
+    // TODO: the way final answer forms are created and submitted needs a refactor
+    if (button.next !== constants.qIDASSEMBLESUCCESS) {
+      ev && ev.preventDefault()
+    }
+    vm.setInterviewAsComplete()
+
+    // disable the previously clicked button
+    setTimeout(() => {
+      $('button:contains(' + button.label + ')').prop('disabled', true)
+    })
+  },
+
+  handleBackButton (rState, button, logic) {
+    // last visited page always at index 1
+    const priorQuestionName = rState.visitedPages[1].name
+    // override with new gotoPage
+    logic.attr('gotoPage', priorQuestionName)
+    button.attr('next', priorQuestionName)
   },
 
   // navigate util functions

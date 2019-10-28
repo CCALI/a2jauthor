b5527c13481849f8d827f78fb590edbd7f7e3d9e
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2019-10-28 12:45:30 -0500

    always set repeatVar if it exists

--- js/viewer/mobile/pages/pages-vm.js
+++ js/viewer/mobile/pages/pages-vm.js
@@ -199,11 +199,9 @@ export default CanMap.extend('PagesVM', {
   },
 
   navigate (button, el, ev) {
-    const vm = this
-    vm.traceButtonClicked(button.attr('label'))
-
-    const page = vm.attr('currentPage')
+    const vm = this // preserve navigate context
     const anyFieldHasError = vm.validateAllFields()
+    vm.traceButtonClicked(button.attr('label'))
 
     // do nothing if there are field(s) with error(s) - prevent form submission events
     if (anyFieldHasError) {
@@ -211,54 +209,45 @@ export default CanMap.extend('PagesVM', {
       return false
     } else { // no errors/normal navigation
       const rState = vm.attr('rState')
+      const page = vm.attr('currentPage')
       const logic = vm.attr('logic')
 
-      // Author Preview Mode changes handling of special buttons, and does not post answers
-      if (rState.previewActive && vm.hasSpecialButton(button)) {
-        ev && ev.preventDefault()
-        vm.previewActiveResponses(button, ev)
-      }
-
-      // RESUME button skips rest of navigate
-      if (button.next === constants.qIDRESUME) {
-        vm.resumeInterview()
-        return
-      }
-
-      // special destination qIDFAIL button skips rest of navigate
-      // Author can provide an external URL to explain why user did not qualify
-      if (button.next === constants.qIDFAIL) {
-        vm.handleFailButton(button, vm)
+      // qIDFAIL & qIDRESUME buttons skip rest of navigate
+      if (button.next === constants.qIDFAIL || button.next === constants.qIDRESUME) {
+        vm.handleFailOrResumeButton(button)
         return
       }
 
       // Set answers for buttons with values
       if (button.name) {
-        vm.handleButtonValue(button, vm, page, logic)
+        vm.saveButtonValue(button, vm, page, logic)
       }
 
       // handle afterLogic
-      vm.handleCodeAfter(button, page, logic, vm)
+      vm.handleCodeAfter(button, vm, page, logic)
 
-      // Don't post to the server in Author Preview aka previewActive
+      // set/increment counting variables
+      vm.setRepeatVariable(button)
+
+      // Author Preview Mode changes handling of special buttons, and does not post answers
+      if (rState.previewActive && vm.hasSpecialButton(button)) {
+        ev && ev.preventDefault()
+        vm.previewActiveResponses(button, ev) // preview messages
+      }
       if (!rState.previewActive && (vm.shouldPostOrAssemble(button))) {
-        vm.handleServerPost(rState, button, vm, ev)
-      }
-
-      // user has selected to navigate to a prior question
-      if (button.next === constants.qIDBACK) {
-        vm.handleBackButton(rState, button, logic)
-      }
-
-      vm.handleGotoOrNormalNav(rState, button, logic, vm)
-      // if these special buttons are used, the interview is complete (incomplete is false)
-      if (vm.shouldCompleteInterview(button)) {
         vm.setInterviewAsComplete()
+        vm.handleServerPost(button, vm, rState, ev) // normal post/assemble
       }
+
+      if (button.next === constants.qIDBACK) { // nav to prior question
+        vm.handleBackButton(button, rState, logic)
+      }
+
+      vm.handleGotoOrNormalNav(button, vm, rState, logic)
     }
   },
 
-  handleGotoOrNormalNav (rState, button, logic, vm) {
+  handleGotoOrNormalNav (button, vm, rState, logic) {
     const gotoPage = logic.attr('gotoPage')
     const logicPageIsNotEmpty = _isString(gotoPage) && gotoPage.length
 
@@ -271,25 +260,22 @@ export default CanMap.extend('PagesVM', {
     // only navigate to the `button.next` page if the button clicked is not
     // any of the buttons with "special" behavior.
     } else if (!vm.hasSpecialButton(button)) {
-      // first update any repeatVars set by buttons, setting to or incrementing by 1
-      const buttonRepeatVar = button.attr('repeatVar')
-      const buttonRepeatVarSet = button.attr('repeatVarSet')
-      if (buttonRepeatVar && buttonRepeatVarSet) {
-        vm.setRepeatVariable(buttonRepeatVar, buttonRepeatVarSet)
-      }
-
       // nav to next page
       rState.page = button.next
     }
   },
 
-  handleFailButton (button, vm) {
+  handleFailOrResumeButton (button, vm) {
+    if (button.next === constants.qIDRESUME) {
+      vm.resumeInterview()
+      return
+    }
+    // Author can provide an external URL to explain why user did not qualify/failed out
     vm.setInterviewAsComplete()
     let failURL = button.url.toLowerCase()
     let hasProtocol = failURL.indexOf('http') === 0
     failURL = hasProtocol ? failURL : 'http://' + failURL
-    if (failURL === 'http://') {
-      // If Empty, standard message
+    if (failURL === 'http://') {// If Empty, standard message
       vm.attr('modalContent', {
         title: 'You did not Qualify',
         text: 'Unfortunately, you did not qualify to use this A2J Guided Interview. Please close your browser window or tab to exit the interview.'
@@ -303,7 +289,7 @@ export default CanMap.extend('PagesVM', {
     }
   },
 
-  handleButtonValue (button, vm, page, logic) {
+  saveButtonValue (button, vm, page, logic) {
     const buttonAnswer = vm.__ensureFieldAnswer(button)
     let buttonAnswerIndex = 1
 
@@ -325,7 +311,7 @@ export default CanMap.extend('PagesVM', {
     buttonAnswer.attr('values.' + buttonAnswerIndex, buttonValue)
   },
 
-  handleCodeAfter (button, page, logic, vm) {
+  handleCodeAfter (button, vm, page, logic) {
     const codeAfter = page.attr('codeAfter')
     // default next page is derived from the button pressed.
     // might be overridden by the After logic or special
@@ -344,7 +330,7 @@ export default CanMap.extend('PagesVM', {
     }
   },
 
-  handleServerPost (rState, button, vm, ev) {
+  handleServerPost (button, vm, rState, ev) {
     // This modal and disable is for LHI/HotDocs issue taking too long to process
     // prompting users to repeatedly press submit, crashing HotDocs
     // Matches A2J4 functionality, but should really be handled better on LHI's server
@@ -361,7 +347,6 @@ export default CanMap.extend('PagesVM', {
     if (button.next !== constants.qIDASSEMBLESUCCESS) {
       ev && ev.preventDefault()
     }
-    vm.setInterviewAsComplete()
 
     // disable the previously clicked button
     setTimeout(() => {
@@ -369,7 +354,7 @@ export default CanMap.extend('PagesVM', {
     })
   },
 
-  handleBackButton (rState, button, logic) {
+  handleBackButton (button, rState, logic) {
     // last visited page always at index 1
     const priorQuestionName = rState.visitedPages[1].name
     // override with new gotoPage
@@ -390,13 +375,6 @@ export default CanMap.extend('PagesVM', {
     return button.next === constants.qIDSUCCESS ||
     button.next === constants.qIDASSEMBLESUCCESS ||
     button.next === constants.qIDASSEMBLE
-  },
-
-  shouldCompleteInterview (button) {
-    return button.next === constants.qIDFAIL ||
-        button.next === constants.qIDSUCCESS ||
-        button.next === constants.qIDASSEMBLE ||
-        button.next === constants.qIDASSEMBLESUCCESS
   },
 
   setInterviewAsComplete () {
@@ -558,31 +536,36 @@ export default CanMap.extend('PagesVM', {
     })
   },
 
-  setRepeatVariable (repeatVar, repeatVarSet) {
-    const logic = this.attr('logic')
-    const traceMessage = this.attr('rState.traceMessage')
-    const traceMsg = {}
+  setRepeatVariable (button) {
+    const repeatVar = button.attr('repeatVar')
+    const repeatVarSet = button.attr('repeatVarSet')
 
-    switch (repeatVarSet) {
-      case constants.RepeatVarSetOne:
-        if (!logic.varExists(repeatVar)) {
-          logic.varCreate(repeatVar, 'Number', false, 'Repeat variable index')
-        }
+    if (repeatVar && repeatVarSet) {
+      const logic = this.attr('logic')
+      const traceMessage = this.attr('rState.traceMessage')
+      const traceMsg = {}
 
-        logic.varSet(repeatVar, 1)
-        traceMsg.key = repeatVar + '-0'
-        traceMsg.fragments = [{ format: '', msg: 'Setting [' + repeatVar + '] to 1' }]
-        break
+      switch (repeatVarSet) {
+        case constants.RepeatVarSetOne:
+          if (!logic.varExists(repeatVar)) {
+            logic.varCreate(repeatVar, 'Number', false, 'Repeat variable index')
+          }
 
-      case constants.RepeatVarSetPlusOne:
-        const value = logic.varGet(repeatVar)
+          logic.varSet(repeatVar, 1)
+          traceMsg.key = repeatVar + '-0'
+          traceMsg.fragments = [{ format: '', msg: 'Setting [' + repeatVar + '] to 1' }]
+          break
 
-        logic.varSet(repeatVar, value + 1)
-        traceMsg.key = repeatVar + '-' + value
-        traceMsg.fragments = [{ format: '', msg: 'Incrementing [' + repeatVar + '] to ' + (value + 1) }]
-        break
+        case constants.RepeatVarSetPlusOne:
+          const value = logic.varGet(repeatVar)
+
+          logic.varSet(repeatVar, value + 1)
+          traceMsg.key = repeatVar + '-' + value
+          traceMsg.fragments = [{ format: '', msg: 'Incrementing [' + repeatVar + '] to ' + (value + 1) }]
+          break
+      }
+
+      traceMessage.addMessage(traceMsg)
     }
-
-    traceMessage.addMessage(traceMsg)
   }
 })

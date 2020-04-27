dc4f654cc85802377327fc7c36fc026c751d73b3
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-04-27 12:20:16 -0500

    update version 2020-04-27

--- js/viewer/models/constants.js
+++ js/viewer/models/constants.js
@@ -7,7 +7,7 @@ export default {
   AJAXLoader: '<span class="loader">&nbsp;</span>',
 
   A2JVersionNum: '6.0', // VersionInfo.verNum
-  A2JVersionDate: '2020-02-17',
+  A2JVersionDate: '2020-04-2',
 
   // CAVersionNum:'5.0.0',
   // CAVersionDate:'2013-04-15',

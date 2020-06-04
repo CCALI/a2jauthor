2e314de14d0426ccf26ef94ed9e8a741492671db
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-06-04 16:25:57 -0500

    linting fixes

--- styles/styles.js
+++ styles/styles.js
@@ -1,3 +1,3 @@
-import 'a2jauthor/styles/author.jquery-ui.css!';
-import 'a2jauthor/styles/jquery.fileupload-ui.css!';
-import 'a2jauthor/styles/A2J_Author.css!';
+import 'a2jauthor/styles/author.jquery-ui.css'
+import 'a2jauthor/styles/jquery.fileupload-ui.css'
+import 'a2jauthor/styles/A2J_Author.css'

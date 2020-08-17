d2567f27bca72028a865b9ff3553cae9afe9718d
mitch <mitch@mukungfu.com>
2020-08-17 11:19:26 -0500

    move own copy of footer from a2jdeps

---
+++ src/footer/footer.js
@@ -0,0 +1,35 @@
+import Component from 'can-component'
+import template from './footer.stache'
+import CanMap from 'can-map'
+import constants from '~/src/models/constants'
+import moment from 'moment'
+
+import 'can-map-define'
+
+let FooterVM = CanMap.extend({
+  define: {
+    authorVersion: {
+      get () {
+        return 'A2J ' + constants.A2JVersionNum + '-' + constants.A2JVersionDate
+      }
+    },
+    currentYear: {
+      get () {
+        return moment().year()
+      }
+    },
+    // Used to hide status updates in Author preview mode
+    showCAJAStatus: {
+      get () {
+        return !this.attr('rState.previewActive')
+      }
+    }
+  }
+})
+
+export default Component.extend({
+  view: template,
+  ViewModel: FooterVM,
+  leakScope: false,
+  tag: 'author-footer'
+})

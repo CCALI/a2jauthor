820122399bd5e7b5adc28b638659654eb8735854
mitch <mitch@mukungfu.com>
2020-08-20 08:07:29 -0500

    update version for release 2020-08-20

--- legacy/viewer/A2J_Types.js
+++ legacy/viewer/A2J_Types.js
@@ -23,8 +23,8 @@ var CONST = {
   // Spinnner for loading wait
   AJAXLoader: '<span class="loader">&nbsp;</span>"',
 
-  A2JVersionNum: '6.0', // VersionInfo.verNum
-  A2JVersionDate: '2020-08-15stage',
+  A2JVersionNum: '7.0', // VersionInfo.verNum
+  A2JVersionDate: '2020-08-20',
 
   // CAVersionNum:"5.0.0",
   // CAVersionDate:"2013-04-15",

85594416f66f3c6dbb5078dba66f1ad8fb790ea6
Mike 'mitch' Mitchel <dev@mukungfu.com>
2020-11-17 13:45:09 -0600

    88 handle Author set step numbers (#89)
    * handle Author set step numbers
    
    * make stepKey more explicit
    
    * fix template toolbar input height

--- src/mapper/mapper.js
+++ src/mapper/mapper.js
@@ -46,8 +46,12 @@ export const MapperVM = DefineMap.extend('MapperVM', {
         // TODO: restore after popup step refactor
         // pagesAndPopups['popups'].pages.push(page)
       } else {
-        const stepKey = `${page.step}`
-        pagesAndPopups[stepKey].pages.push(page)
+        // page.step is the index for a Step in the Steps array, which is not always the same as the step number
+        const step = guideSteps[page.step]
+        const stepKey = `${step.number}`
+        if (pagesAndPopups[stepKey] && pagesAndPopups[stepKey].pages) {
+          pagesAndPopups[stepKey].pages.push(page)
+        }
       }
     })
 

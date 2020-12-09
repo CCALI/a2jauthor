15636a2321dc7ec23e14cece541d758151de20c9
mitch <mitch@mukungfu.com>
2020-12-09 10:42:28 -0600

    refactor about-revision to canjs

---
+++ src/about/revision/revision-test.js
@@ -0,0 +1,25 @@
+// TODO: add this in to app level tests if needed
+
+// import { AboutRevisionVM } from './revision'
+// import { assert } from 'chai'
+// import stache from 'can-stache'
+// import canViewModel from 'can-view-model'
+
+// import 'steal-mocha'
+
+// describe('<about-revision>', () => {
+//   describe('Component', () => {
+//     const render = (data) => {
+//       const tpl = stache('<about-revision guide:from="guide" />')
+//       document.querySelector('#test-area').appendChild(tpl(data))
+//       return canViewModel('about-revision')
+//     }
+
+//     const vm = render({ guide: { sendrevision: false, emailContact: '' } })
+
+//     it('setGuideProp', () => {
+//       vm.setGuideProp('logoImage', 'foo')
+//       assert.equal(vm.guide.attr('logoImage'), 'foo', 'should set the guide property')
+//     })
+//   })
+// })

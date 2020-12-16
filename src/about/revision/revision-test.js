ae05f9a33d471dd90d40757e4d32ae1c5b1b0fdc
mitch <mitch@mukungfu.com>
2020-12-16 17:50:52 -0600

    add basic test

--- src/about/revision/revision-test.js
+++ src/about/revision/revision-test.js
@@ -1,25 +1,39 @@
 // TODO: add this in to app level tests if needed
 
-// import { AboutRevisionVM } from './revision'
-// import { assert } from 'chai'
-// import stache from 'can-stache'
-// import canViewModel from 'can-view-model'
+import { AboutRevisionVM } from './revision'
+import { assert } from 'chai'
+import CanMap from 'can-map'
+import stache from 'can-stache'
+import canViewModel from 'can-view-model'
 
-// import 'steal-mocha'
+import 'steal-mocha'
 
-// describe('<about-revision>', () => {
-//   describe('Component', () => {
-//     const render = (data) => {
-//       const tpl = stache('<about-revision guide:from="guide" />')
-//       document.querySelector('#test-area').appendChild(tpl(data))
-//       return canViewModel('about-revision')
-//     }
+describe('<about-revision>', () => {
+  describe('Component', () => {
+    it('connectedCallback', (done) => {
+      // no op for domMutate
+      window.can = {
+        domMutate: {
+          onNodeRemoval: () => { return false }
+        }
+      }
 
-//     const vm = render({ guide: { sendrevision: false, emailContact: '' } })
+      const render = (data) => {
+        const tpl = stache('<about-revision guide:from="guide" />')
+        document.querySelector('#test-area').appendChild(tpl(data))
+        return canViewModel('about-revision')
+      }
 
-//     it('setGuideProp', () => {
-//       vm.setGuideProp('logoImage', 'foo')
-//       assert.equal(vm.guide.attr('logoImage'), 'foo', 'should set the guide property')
-//     })
-//   })
-// })
+      const guide = new CanMap({ notes: 'start', sendrevision: false, emailContact: '', version: '' })
+      const vm = render({ guide })
+
+      setTimeout(() => {
+        const ckeDiv = document.querySelector('.htmledit')
+        const divNotes = ckeDiv.textContent
+
+        assert.equal(divNotes, vm.guide.attr('notes'), 'should restore current notes from guide')
+        done()
+      })
+    })
+  })
+})

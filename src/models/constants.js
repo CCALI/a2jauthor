5d183bef6285efdd758de17da18e8fcc12e8c205
Tobias Nteireho <tobias@cali.org>
2022-02-08 09:10:35 -0600

    Update constants.js

--- src/models/constants.js
+++ src/models/constants.js
@@ -9,7 +9,7 @@ export default {
   AJAXLoader: '<span class="loader">&nbsp;</span>',
 
   A2JVersionNum: '8.0.2', // VersionInfo.verNum
-  A2JVersionDate: '2022-01-19',
+  A2JVersionDate: '2022-02-08',
 
   // CAVersionNum:'5.0.0',
   // CAVersionDate:'2013-04-15',

fa2d1fee214fedef598e354d22cf71004d815560
mitch <mitch@mukungfu.com>
2022-03-24 12:12:59 -0500

    add ckeditor plugin 'textindent'

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -3,29 +3,32 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function (config) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	// extraPlugins defined in a2j-deps: a2j-repeat-loop.js, a2j-rich-text.js
 	config.removePlugins = 'smiley';
+
 	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+		{ name: 'forms', groups: ['forms'] },
 		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'textindent', 'paragraph'] },
+		{ name: 'links', groups: ['links'] },
+		{ name: 'insert', groups: ['insert'] },
 		'/',
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
+		{ name: 'styles', groups: ['styles'] },
+		{ name: 'colors', groups: ['colors'] },
+		{ name: 'tools', groups: ['tools'] },
+		{ name: 'others', groups: ['others'] },
+		{ name: 'about', groups: ['about'] }
 	];
 
 	config.removeButtons = 'Image,ImageButton,Paste,PasteText,PasteFromWord,Anchor,Iframe,Flash,CreateDiv,HiddenField,Form,Checkbox,Radio,TextField,Textarea,Select,Button';
 
+	console.log('setting config', config)
 };

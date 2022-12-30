23cdcd279d0173f48689c91cd31f5c9e87229ed8
Tobias Nteireho <tobias@cali.org>
2022-12-30 15:37:52 -0600

    intentional fail

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -11,7 +11,7 @@ const FooterVM = CanMap.extend({
     authorVersion: {},
     currentYear: {
       get () {
-        return moment().year()
+        return moment().yer();
       }
     },
     // Used to hide status updates in Author preview mode

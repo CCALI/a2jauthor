66f46543307750ea1c66e6f3a71767adf7182d63
mitch <mitch@nomadiklabs.com>
2024-08-22 12:19:29 -0400

    set default message

--- src/guide-health/guide-health.js
+++ src/guide-health/guide-health.js
@@ -2,9 +2,10 @@ import DefineMap from 'can-define/map/map'
 import Component from 'can-component'
 import template from './guide-health.stache'
 
-export const GuideHealthVM = DefineMap.extend('FielGuideHealthVMdVM', {
+export const GuideHealthVM = DefineMap.extend('GuideHealthVM', {
   message: {
-    type: 'text'
+    type: 'text',
+    default: ''
   }
 })
 

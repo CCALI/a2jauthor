0f9995f05506468ac8912766a2b1f92eba00c3f9
Tobias Nteireho <tobias@cali.org>
2024-09-13 10:32:19 -0500

    cleanup

--- bad-answer-alert.php
+++ bad-answer-alert.php
@@ -1,23 +1,40 @@
 <?php
 
-if ((empty($_REQUEST["interviewid"])) || (strlen($_REQUEST["interviewid"]) === 0) ){
-    error_log('bad feedback: '. $_SERVER['HTTP_USER_AGENT', 0);
-    die();
-}
-
-
 $config = parse_ini_file($path . '/config_env.ini');
 $service_email = $config['SERVICE_EMAIL'];
 
 
+$keys = ["interviewtitle", "viewerversion", 
+             "type", "authoremail"]; 
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+function checkRequest($keys, $user_agent){
+
+    foreach ($keys as $key){
+        if ((empty($_REQUEST[$key])) || 
+        (strlen($_REQUEST[$key]) === 0) ){
+            return false;
+        }
+    }
+    return true;
+}
+
+if (checkRequest($keys, $user_agent)){
+    error_log('bad viewer variable alert: '. $user_agent,0);
+            die();
+}
+
+$interviewtitle= ($_REQUEST["interviewtitle"]);
+$viewerversion=  ($_REQUEST["viewerversion"]);
+$variables =   ($_REQUEST["type"]);
+$authoremail =  ($_REQUEST["authoremail"]);
+$created=$now=date("Y-m-d-H-i-s");
+
+
 $message="
-An A2J user has submitted feedback using the A2J Viewer Feedback form:
+An A2J user has submitted an answerset with invalid dates or numbers:
     <ul>
-    <li>User email (optional): $email
-    <li>Feedback type: $type
-    <li>Feedback: $comment
     <li>Interview Title: " . htmlentities(stripslashes($interviewtitle)) . "
-    <li>Interview Question: $questionid
     <li>Interview URL: " . htmlentities(stripslashes($interviewid)). "
     <li>Viewer Version: $viewerversion
     <li>User Agent: $user_agent

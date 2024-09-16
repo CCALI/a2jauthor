6fcdafd10d854e8f10ea727ba2fee066466854a6
mitch <mitch@nomadiklabs.com>
2024-09-15 21:44:28 -0400

    update messages in pages and reports tab

--- src/pages-tab/components/question-info/question-info.js
+++ src/pages-tab/components/question-info/question-info.js
@@ -7,23 +7,6 @@ export const QuestionInfoVM = DefineMap.extend('QuestionInfoVM', {
   page: {},
   appState: {},
   guideFiles: {},
-  get vars () {
-    return this.appState.guide.vars
-  },
-
-  get showCountingVariableWarning () {
-    const repeatVarName = this.page && this.page.repeatVar && this.page.repeatVar.toLowerCase()
-    return !!repeatVarName && this.vars[repeatVarName].type !== 'Number'
-  },
-
-  get showOuterLoopVariableWarning () {
-    const outerLoopVarName = this.page && this.page.outerLoopVar && this.page.outerLoopVar.toLowerCase()
-    return !!outerLoopVarName && this.vars[outerLoopVarName].type !== 'Number'
-  },
-
-  healthMessage: {
-    default: () => 'Counting Variables require Variable Type (Number)'
-  },
 
   get ckeText () {
     return ckeFactory(this.page, 'text', 'Text:')
